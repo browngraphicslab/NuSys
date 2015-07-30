06faecca6878629ba78b5cf149740dcdce178214
Adil Virani <me@adilvirani.io>
2015-07-30 11:03:02 -0400

    update zoom + ui

--- NuSysApp/Views/WorkspaceView.xaml.cs
+++ NuSysApp/Views/WorkspaceView.xaml.cs
@@ -365,15 +365,24 @@ namespace NuSysApp
 
             var vm = (WorkspaceViewModel)this.DataContext;
             var compositeTransform = vm.CompositeTransform;
-            var center = compositeTransform.Inverse.TransformPoint(e.GetCurrentPoint(this).Position);
+     
+
 
             Debug.WriteLine(((double)e.GetCurrentPoint(this).Properties.MouseWheelDelta +240)/240);
-            compositeTransform.ScaleX *= (3+((double)e.GetCurrentPoint(this).Properties.MouseWheelDelta +240)/240)/4;
-            compositeTransform.ScaleY *= (3+((double)e.GetCurrentPoint(this).Properties.MouseWheelDelta +240)/ 240)/4;
 
-            compositeTransform.CenterX = center.X;
+            //////////////
+            var zoomspeed = 4;
+            var delta = ((3 + ((double) e.GetCurrentPoint(this).Properties.MouseWheelDelta + 240)/240) - 4)/zoomspeed;
+            if (compositeTransform.ScaleX + delta > 0)
+            {
+                var center = compositeTransform.Inverse.TransformPoint(e.GetCurrentPoint(this).Position);
+                compositeTransform.ScaleX += delta;
+                compositeTransform.ScaleY += delta;
+                compositeTransform.CenterX = center.X;
             compositeTransform.CenterY = center.Y;
+            }
 
+            Debug.WriteLine(compositeTransform.ScaleX + "!!!!!!!!!!!!!!!!!" + compositeTransform.ScaleY);
             vm.CompositeTransform = compositeTransform;
         }
         private void FM_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
