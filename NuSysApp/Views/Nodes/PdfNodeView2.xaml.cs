1233757b82f0aa7a1ea8fe0b85531a44b6ea92b5
Slymodi <Slymishra@gmail.com>
2015-08-11 10:36:27 -0400

    pdf to new paradigm

---
+++ NuSysApp/Views/Nodes/PdfNodeView2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NuSysApp
+{
+    public sealed partial class PdfNodeView2 : UserControl
+    {
+        public PdfNodeView2(PdfNodeViewModel vm)
+        {
+            this.InitializeComponent();
+            this.DataContext = vm;
+        }
+
+
+        private void OnEditInk(object sender, RoutedEventArgs e)
+        {
+            nodeTpl.ToggleInkMode();
+        }
+    }
+}
