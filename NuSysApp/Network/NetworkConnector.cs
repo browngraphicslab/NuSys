30e5230bfac6097166d1b8e83579b7692f5fd5bd
Trent Green <trent_green@brown.edu>
2015-08-12 17:55:59 -0400

    here are the aforementioned structures for tcp/udp socket networking

---
+++ NuSysApp/Network/NetworkConnector.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace NuSysApp
+{
+    class NetworkConnector
+    {
+        private string _UDPPort = "2156";
+        private string _TCPInputPort = "302";
+        private List<Tuple<DatagramSocket, DataWriter>> _UDPOutSockets;
+        public NetworkConnector()
+        {
+            this.Init();
+        }
+        private async void Init()
+        {
+            _UDPOutSockets = new List<Tuple<DatagramSocket, DataWriter>>();
+            foreach (string ip in GetOtherIPs())
+            {
+                this.AddUDPSocket(ip);
+            }
+
+            StreamSocketListener listener = new StreamSocketListener();
+            listener.ConnectionReceived += this.TCPConnectionRecieved;
+            await listener.BindEndpointAsync(new HostName(this.LocalIPAddress()), _TCPInputPort);
+
+            DatagramSocket socket = new DatagramSocket();
+            socket.MessageReceived += this.DatagramMessageRecieved;
+            socket.ConnectAsync(new HostName(this.LocalIPAddress()), _UDPPort);
+
+        }
+        private async void TCPConnectionRecieved(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            DataReader reader = new DataReader(args.Socket.InputStream);
+            string ip = args.Socket.Information.LocalAddress.RawName;
+            string message;
+            try
+            {
+                uint fieldCount = await reader.LoadAsync(sizeof (uint));
+                if (fieldCount != sizeof (uint))
+                {
+                    Debug.WriteLine("TCP connection recieved at IP "+this.LocalIPAddress()+" but socket closed before full stream was read");
+                    return;
+                }
+                uint stringLength = reader.ReadUInt32();
+                uint actualLength = await reader.LoadAsync(stringLength);
+                message = reader.ReadString(actualLength);
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("Exception caught during TCP connection recieve at IP " + this.LocalIPAddress() + " with error code: " + e.Message);
+                return;
+            }
+        }
+
+        private List<string> GetOtherIPs()
+        {
+            List<string> ips = new List<string>();
+            ips.Add("10.38.22.71");
+            return ips;//TODO add in Phil's php script
+        }
+
+        public string LocalIPAddress()
+        {
+            HostName localHostName = NetworkInformation.GetHostNames().FirstOrDefault(h =>
+                    h.IPInformation != null &&
+                    h.IPInformation.NetworkAdapter != null);
+            return localHostName.RawName;
+        }
+
+        private void AddUDPSocket(string ip)
+        {
+            DatagramSocket socket = new DatagramSocket();
+            socket.ConnectAsync(new HostName(ip), _UDPPort);
+            DataWriter writer =  new DataWriter(socket.OutputStream);
+            _UDPOutSockets.Add(new Tuple<DatagramSocket,DataWriter>(socket,writer));
+        }
+        private async void DatagramMessageRecieved(DatagramSocket sender, DatagramSocketMessageReceivedEventArgs args)
+        {
+            string ip = sender.Information.LocalAddress.RawName;
+            string message;
+            try
+            {
+                var result = args.GetDataStream();
+                var resultStream = result.AsStreamForRead(1024);
+
+                using (var reader = new StreamReader(resultStream))
+                {
+                    message = await reader.ReadToEndAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception caught during message recieve at IP "+this.LocalIPAddress()+" with error code: "+e.Message);
+                return;
+            }
+        }
+        public async void SendTCPMessage(string message, string recievingIP, string outport)
+        {
+            try
+            {
+                StreamSocket socket = new StreamSocket();
+                await socket.ConnectAsync(new HostName(recievingIP), outport);
+                DataWriter writer = new DataWriter(socket.OutputStream);
+                writer.WriteUInt32(writer.MeasureString(message));
+                writer.WriteString(message);
+                await writer.StoreAsync();
+                writer.Dispose();
+                socket.Dispose();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception caught during TCP message send at IP " + this.LocalIPAddress() + " with error code: " + e.Message);
+                return;
+            }
+        }
+
+        public async Task sendMassUDPMessage(string message)
+        {
+            foreach (Tuple<DatagramSocket,DataWriter> tup in this._UDPOutSockets)
+            {
+                await this.SendUDPMessage(message, tup.Item1, tup.Item2);
+            }
+        }
+        private async Task SendUDPMessage(string message, DatagramSocket socket, DataWriter writer)
+        {
+            writer.WriteString(message);
+            await writer.StoreAsync();
+        }
+    }
+}
---
+++ NuSysApp/WaitingRoom/WaitingRoomView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NuSysApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WaitingRoomView : Page
+    {
+        private NetworkConnector _networkConnector;
+        public WaitingRoomView()
+        {
+            this.InitializeComponent();
+            Init();
+        }
+
+        public async void Init()
+        {
+            _networkConnector = new NetworkConnector();   
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(WorkspaceView));
+        }
+    }
+}
