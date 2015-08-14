b86aafcaa5acb70f43d44b1d13ed31ae0b78f0b2
Gary Chien <garychien1@gmail.com>
2015-08-14 10:33:25 -0400

    Disabled Resharper namespace location warning by setting the Namespace Provider of all folders to False

--- NuSysApp/MISC/CortanaContinuousRecognizer.cs
+++ NuSysApp/MISC/CortanaContinuousRecognizer.cs
@@ -17,13 +17,16 @@ namespace NuSysApp
         {
             _dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
             _speechRecognizer = new SpeechRecognizer();
-            var result = await _speechRecognizer.CompileConstraintsAsync();
+            await _speechRecognizer.CompileConstraintsAsync();
             _speechRecognizer.ContinuousRecognitionSession.ResultGenerated +=
                 ContinuousRecognitionSession_ResultGenerated;
-            _speechRecognizer.ContinuousRecognitionSession.Completed += ContinuousRecognitionSession_Completed;
+            _speechRecognizer.ContinuousRecognitionSession.Completed +=
+                ContinuousRecognitionSession_Completed;
+
         }
 
-        private async void ContinuousRecognitionSession_ResultGenerated(SpeechContinuousRecognitionSession sender,
+        private async void ContinuousRecognitionSession_ResultGenerated(
+            SpeechContinuousRecognitionSession sender,
             SpeechContinuousRecognitionResultGeneratedEventArgs args)
         {
             if (args.Result.Confidence == SpeechRecognitionConfidence.Medium ||
@@ -44,26 +47,28 @@ namespace NuSysApp
             }
         }
 
-        private async void ContinuousRecognitionSession_Completed(SpeechContinuousRecognitionSession sender,
+        private async void ContinuousRecognitionSession_Completed(
+            SpeechContinuousRecognitionSession sender,
             SpeechContinuousRecognitionCompletedEventArgs args)
         {
-            if (args.Status != SpeechRecognitionResultStatus.Success)
+            switch (args.Status)
             {
-                if (args.Status == SpeechRecognitionResultStatus.TimeoutExceeded)
-                {
+                case SpeechRecognitionResultStatus.Success:
+                    return;
+                case SpeechRecognitionResultStatus.TimeoutExceeded:
                     await _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
                     {
                         Debug.WriteLine("Automatic timeout of dictation");
                         var text = _dictatedTextBuilder.ToString();
                     });
-                }
-                else
-                {
+                    break;
+                default:
                     await _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
                     {
-                        Debug.WriteLine("Continuous Recognition Completed: " + args.Status.ToString());
+                        Debug.WriteLine("Continuous Recognition Completed: " 
+                            + args.Status.ToString());
                     });
-                }
+                    break;
             }
         }
     }
