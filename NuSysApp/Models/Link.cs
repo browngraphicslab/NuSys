00a360ba15d3cc123520d168b100b4711820d293
Carlene Niguidula <carlene_niguidula@brown.edu>
2015-08-17 15:49:28 -0400

    fixed link model and vm refactoring

--- NuSysApp/Models/Link.cs
+++ NuSysApp/Models/Link.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Xml;
+
 namespace NuSysApp
 {
     public class Link : Atom
@@ -13,7 +15,26 @@ namespace NuSysApp
         public int InAtomID { get; set; }
         public int OutAtomID { get; set; }
 
-        public AtomViewModel atom1 { get; set; }
-        public AtomViewModel atom2 { get; set; }
+        public Atom atom1 { get; set; }
+        public Atom atom2 { get; set; }
+
+        public XmlElement WriteXML(XmlDocument doc)
+        {
+
+            //XmlElement 
+            XmlElement link = doc.CreateElement(string.Empty, "Link", string.Empty); //TODO: Change how we determine node type for name
+
+            //Atoms that this link is bound to
+            XmlAttribute id1 = doc.CreateAttribute("atomID1");
+            id1.Value = InAtomID.ToString();
+            link.SetAttributeNode(id1);
+
+            XmlAttribute id2 = doc.CreateAttribute("atomID2");
+            id2.Value = OutAtomID.ToString();
+            link.SetAttributeNode(id2);
+
+            return link;
+        }
+
     }
 }
