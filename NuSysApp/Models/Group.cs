57f5cd842e92d1cc384e9c5c43bec339b0db40d0
Carlene Niguidula <carlene_niguidula@brown.edu>
2015-08-17 14:19:51 -0400

    Added group model file

---
+++ NuSysApp/Models/Group.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Windows.UI.Xaml.Controls;
+
+namespace NuSysApp.Models
+{
+    public class Group : Node
+    {
+        public Group(int id): base(id)
+        {
+
+        }
+
+        public ObservableCollection<UserControl> AtomViewList { get; set; }
+
+        public ObservableCollection<NodeViewModel> NodeViewModelList { get; set; }
+
+        public ObservableCollection<LinkViewModel> LinkViewModelList { get; set; }
+
+        public override XmlElement WriteXML(XmlDocument doc)
+        {
+
+            //Main XmlElement 
+            XmlElement groupNode = doc.CreateElement(string.Empty, "Node", string.Empty); //TODO: Change how we determine node type for name
+
+
+            //Other attributes - id, x, y, height, width
+            List<XmlAttribute> basicXml = this.getBasicXML(doc);
+            foreach (XmlAttribute attr in basicXml)
+            {
+                groupNode.SetAttributeNode(attr);
+            }
+
+            //get nodes within groups
+            foreach (NodeViewModel nodevm in NodeViewModelList)
+            {
+                groupNode.AppendChild(nodevm.WriteXML(doc));
+            }
+            return groupNode;
+        }
+    }
+}
