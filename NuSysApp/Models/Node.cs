9dbe2b99f0fddc52c870a04cb1bf8e6c5c56b8f0
Tiffany Citra <tiffany.citra@yahoo.com>
2015-08-20 12:34:35 -0400

    loading workspace

--- NuSysApp/Models/Node.cs
+++ NuSysApp/Models/Node.cs
@@ -52,7 +52,6 @@ namespace NuSysApp
             }
         }
 
-
         public double Y
         {
             get
@@ -129,9 +128,7 @@ namespace NuSysApp
 
         public Constants.NodeType NodeType { get; set; }
 
-        public Group ParentGroup {
-            get; set;
-            }
+        public Group ParentGroup { get; set;}
         
 
         public bool IsAnnotation { get; set; }
@@ -212,11 +209,12 @@ namespace NuSysApp
             }
 
             XmlAttribute x = doc.CreateAttribute("x");
-
             x.Value = X.ToString();
+            basicXml.Add(x);
 
             XmlAttribute y = doc.CreateAttribute("y");
             y.Value = Y.ToString();
+            basicXml.Add(y);
 
 
             XmlAttribute height = doc.CreateAttribute("height");
