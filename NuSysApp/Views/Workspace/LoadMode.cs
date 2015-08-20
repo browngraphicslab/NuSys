ac17f69bd83c632ba5b6e0e51e78e2a54e526d26
Tiffany Citra <tiffany.citra@yahoo.com>
2015-08-20 13:31:16 -0400

    add new files

---
+++ NuSysApp/Views/Workspace/LoadMode.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace NuSysApp.Views.Workspace
+{
+    public class LoadMode : AbstractWorkspaceViewMode
+    {
+        public LoadMode(WorkspaceView view) : base(view)
+        {
+
+        }
+
+        public override async Task Activate()
+        {
+            var vm = (WorkspaceViewModel)_view.DataContext;
+            vm.LoadWorkspace();
+        }
+
+        public override async Task Deactivate()
+        {
+
+        }
+
+    }
+}
