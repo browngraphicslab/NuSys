32b74ffbe146d9bd2090604472c73fa2dd75b7bd
nickturley <nicholas_turley@brown.edu>
2015-08-22 16:15:03 -0400

    added some stuffzzz

---
+++ NuSysApp/EventArgs/CreateEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class CreateEventArgs : System.EventArgs
+    {
+        private readonly string _eventInfo;
+        private Node _node;
+
+        public CreateEventArgs(string text, Node node)
+        {
+            _eventInfo = text;
+            _node = node;
+        }
+
+        public string GetInfo()
+        {
+            return _eventInfo;
+        }
+
+        public Node CreatedNode => _node;
+    }
+}
---
+++ NuSysApp/EventArgs/LinkedEventArgs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class LinkedEventArgs : System.EventArgs
+    {
+        private readonly string _eventInfo;
+        private Link _link;
+
+        public LinkedEventArgs(string text, Link link)
+        {
+            _eventInfo = text;
+            _link = link;
+        }
+
+        public Atom Atom1
+        {
+            get { return _link.Atom1; }
+        }
+
+        public Atom Atom2
+        {
+            get { return _link.Atom2; }
+        }
+
+        public string ID
+        {
+            get { return _link.ID; }
+        }
+        public string GetInfo()
+        {
+            return _eventInfo;
+        }
+
+        
+    }
+}
