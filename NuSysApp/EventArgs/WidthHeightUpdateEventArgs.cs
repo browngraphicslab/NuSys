bff687c4d2bce29994a77bb1d57c1580d4465c43
nickturley <nicholas_turley@brown.edu>
2015-08-22 17:23:19 -0400

    cleaned up the way event notification works

---
+++ NuSysApp/EventArgs/WidthHeightUpdateEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class WidthHeightUpdateEventArgs: EventArgs
+    {
+        private readonly string _eventInfo;
+
+        public WidthHeightUpdateEventArgs(string text, double width, double height)
+        {
+            _eventInfo = text;
+            Width = width;
+            Height = height;
+        }
+
+        public string GetInfo()
+        {
+            return _eventInfo;
+        }
+
+        public double Width { get; }
+
+        public double Height { get; }
+    }
+}
