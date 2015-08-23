0b5ca82a578cf88121486747a86e1fd3b1d30f73
nickturley <nicholas_turley@brown.edu>
2015-08-23 17:01:00 -0400

    added my sheeet

---
+++ NuSysApp/EventArgs/CreateGroupEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class CreateGroupEventArgs
+    {
+        private readonly string _eventInfo;
+
+        public CreateGroupEventArgs(string text, Group group)
+        {
+            _eventInfo = text;
+            CreatedGroup = group;
+        }
+
+        public string GetInfo()
+        {
+            return _eventInfo;
+        }
+
+        public Group CreatedGroup { get; }
+    }
+}
