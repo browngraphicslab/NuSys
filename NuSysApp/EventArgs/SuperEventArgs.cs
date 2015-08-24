35f3e58192dc2d97f40aafc5034fd54bff0eec7e
nickturley <nicholas_turley@brown.edu>
2015-08-24 08:19:16 -0400

    new stuff

---
+++ NuSysApp/EventArgs/SuperEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace NuSysApp
+{
+    public abstract class SuperEventArgs : System.EventArgs
+    {
+        private readonly string _eventInfo;
+
+        protected SuperEventArgs(string text)
+        {
+            _eventInfo = text;
+           
+        }
+
+        public string GetInfo()
+        {
+            return _eventInfo;
+        }
+
+    }
+}
