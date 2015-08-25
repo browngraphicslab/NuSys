15571e5e80d4d64d3edcf2b6a4cd2ad509f43082
Trent Green <trent_green@brown.edu>
2015-08-24 22:59:06 -0400

    added MISSING files

---
+++ NuSysApp/EventArgs/AddPartialLineEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Shapes;
+
+namespace NuSysApp
+{
+    public class AddPartialLineEventArgs : SuperEventArgs
+    {
+        public AddPartialLineEventArgs(string text, IList<Line> lines):base(text)
+        {
+            AddedLines = lines;
+        }
+
+        public IList<Line> AddedLines { get; }
+    }
+}
---
+++ NuSysApp/ViewModels/InqLineViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace NuSysApp
+{
+    public class InqLineViewModel : BaseINPC
+    {
+        private Point _lastAddedPoint;
+        public InqLineViewModel(InqLineModel model)
+        {
+            Model = model;
+        }
+        public InqLineModel Model { get; }
+
+        public Point LastAddedPoint
+        {
+            set
+            {
+                if (_lastAddedPoint == value) return;
+                _lastAddedPoint = value;
+                RaisePropertyChanged("LastAddedPoint");
+            }
+            get { return _lastAddedPoint; }
+        }
+    }
+}
