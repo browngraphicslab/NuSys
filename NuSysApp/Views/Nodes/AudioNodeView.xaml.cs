fcf5443aab047615964f15c1f5683cbb8352b30a
Miranda Chao <miranda_chao@brown.edu>
2015-08-27 14:54:24 -0400

    audio node view

--- NuSysApp/Views/Nodes/AudioNodeView.xaml.cs
+++ NuSysApp/Views/Nodes/AudioNodeView.xaml.cs
@@ -1,8 +1,17 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Windows.Storage;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -10,83 +19,9 @@ namespace NuSysApp
 {
     public sealed partial class AudioNodeView : UserControl
     {
-        //private readonly string _rootFilePath = NuSysStorages.Media.Path + @"\";
-        private readonly AudioNodeViewModel _anvm;
-        private bool _recording;
-        private bool _playing;
-        public AudioNodeView(AudioNodeViewModel vm)
+        public AudioNodeView()
         {
             this.InitializeComponent();
-            this.DataContext = vm;
-            _anvm = vm;
-            _recording = false;
-            _playing = false;
-            CurrentAudioFile = null;
-            playbackButton.IsEnabled = false;
-            recordButton.IsEnabled = true;
-            playbackStatus.Text = "no audio saved";
-            recordingStatus.Text = "click to record";
-        }
-        private string FileName => _anvm.FileName;
-
-        private StorageFile CurrentAudioFile
-        {
-            get { return _anvm.CurrentAudioFile; }
-            set { _anvm.CurrentAudioFile = value; }
-        }
-
-        private async void RecordButton_Click(object sender, TappedRoutedEventArgs e)
-        {
-            await ToggleRecording(FileName + ".mp3");
-        }
-        private async void PlaybackButton_Click(object sender, TappedRoutedEventArgs e)
-        {
-            await TogglePlayback();
-        }
-
-        private async Task ToggleRecording(string fileName)
-        {
-            if (!_recording)
-            {
-                nodeTitle.IsEnabled = false;
-                playbackButton.IsEnabled = false;
-                await _anvm.AudioRecorder.InitializeAudioRecording();
-                CurrentAudioFile = await _anvm.AudioRecorder.CaptureAudio(fileName);
-                _recording = true;
-                recordingStatus.Text = "recording... (click to stop)";
-            }
-            else
-            {
-                await _anvm.AudioRecorder.StopCapture();
-                _recording = false;
-                playbackButton.IsEnabled = true;
-                nodeTitle.IsEnabled = true;
-                recordingStatus.Text = "click to record";
-                playbackStatus.Text = "click to play";
-            }
-        }
-
-        private async Task TogglePlayback()
-        {
-            if (CurrentAudioFile == null) return;
-            var stream = await CurrentAudioFile.OpenAsync(FileAccessMode.Read);
-            playbackElement.SetSource(stream, CurrentAudioFile.FileType);
-            if (!_playing)
-            {
-                nodeTitle.IsEnabled = false;
-                recordButton.IsEnabled = false;
-                playbackElement.Play();
-                _playing = true;
-                playbackStatus.Text = "playing...";
-            }
-            else
-            {
-                playbackElement.Stop();
-                _playing = false;
-                playbackStatus.Text = "click to play";
-                recordButton.IsEnabled = true;
-                nodeTitle.IsEnabled = true;
-            }
         }
     }
 }
