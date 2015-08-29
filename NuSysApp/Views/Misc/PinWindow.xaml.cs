8d26fed33ba4ec9922a52e7e1b294ad49ddab82a
Philipp Eichmann <peichmann@cs.brown.edu>
2015-08-29 17:11:16 -0400

    fixed pin location when navigating through list

--- NuSysApp/Views/Misc/PinWindow.xaml.cs
+++ NuSysApp/Views/Misc/PinWindow.xaml.cs
@@ -26,46 +26,22 @@ namespace NuSysApp
             Border.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(175, 81, 220, 231));           
         }
 
-        public static readonly DependencyProperty pinList = DependencyProperty.Register( "PinList", typeof(ObservableCollection<PinViewModel>),
-          typeof(MinimapView), new PropertyMetadata(new ObservableCollection<PinViewModel>())
-        );
-        public ObservableCollection<PinViewModel> PinList
-        {
-            get { return (ObservableCollection<PinViewModel>)GetValue(pinList); }
-            set
-            {
-                SetValue(pinList, value);
-                //         ((MinimapViewModel)DataContext).PinList = value;
-            }
-        }
+
         private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
         {
             var pinvm = ((TextBlock)sender).DataContext as PinViewModel;
+            var pinModel = (PinModel) pinvm.Model;
+
             var vm = (WorkspaceViewModel)this.DataContext;
 
             var c = new CompositeTransform
             {
                 ScaleX = 1,
                 ScaleY = 1,
-                TranslateX = -pinvm.Transform.Matrix.OffsetX + ((Canvas)this.Parent).ActualWidth / 2,
-                TranslateY = -pinvm.Transform.Matrix.OffsetY + ((Canvas)this.Parent).ActualHeight / 2
+                TranslateX = -pinModel.X + Window.Current.Bounds.Width / 2,
+                TranslateY = -pinModel.Y + Window.Current.Bounds.Height / 2,
             };
             vm.CompositeTransform = c;
         }
-
-        //private void ShowPins(object sender, TappedRoutedEventArgs e)
-        //{
-        //    if (IC2.Visibility == Visibility.Collapsed)
-        //    {
-        //        IC2.Visibility = Visibility.Visible;
-        //        PinButton.Content = "pins -";
-        //    }
-        //    else
-        //    {
-        //        IC2.Visibility = Visibility.Collapsed;
-        //        PinButton.Content = "pins +";
-        //    }
-
-        //}
     }
 }
