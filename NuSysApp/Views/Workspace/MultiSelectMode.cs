f8f427449c3fd649adae7f64a6c6e3745efb5542
ben-sheeran <benjamin_sheeran@brown.edu>
2015-08-31 12:26:37 -0400

    not really anything im just gonna pull

--- NuSysApp/Views/Workspace/MultiSelectMode.cs
+++ NuSysApp/Views/Workspace/MultiSelectMode.cs
@@ -193,7 +193,7 @@ namespace NuSysApp
                     var avm = atom.DataContext as AtomViewModel;
                     if (!avm.IsSelected)
                     {
-                        selected.Add(avm);
+                        
                     }
                 }
             }
