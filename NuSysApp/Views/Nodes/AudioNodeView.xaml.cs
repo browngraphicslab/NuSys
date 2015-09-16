16f389ea4cc4eb8a582a7757269c44949c6f42a6
Gary Chien <garychien1@gmail.com>
2015-09-15 23:15:54 -0400

    moved node view resources to app.xaml

--- NuSysApp/Views/Nodes/AudioNodeView.xaml.cs
+++ NuSysApp/Views/Nodes/AudioNodeView.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -144,10 +145,12 @@ namespace NuSysApp
 
                 if (vm.IsSelected)
                 {
+                    var slideout = (Storyboard)App.Current.Resources["slideout"];
                     slideout.Begin();
                 }
                 else
                 {
+                    var slidein = (Storyboard)App.Current.Resources["slidein"];
                     slidein.Begin();
                 }
             }
