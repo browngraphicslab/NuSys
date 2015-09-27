e25d5a0a18685355f29f74fdbd963afc15445965
Tiffany Citra <tiffany.citra@yahoo.com>
2015-09-27 15:18:01 -0400

    add new files

---
+++ NuSysApp/Models/MainModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class MainModel : AtomModel
+    {
+        private Dictionary<string, WorkSpaceModel> _workspaceIdDict;
+        public MainModel() : base("MAIN_ID")
+        {
+            _workspaceIdDict = new Dictionary<string, WorkSpaceModel>();
+        }
+
+        public Dictionary<string, WorkSpaceModel> IDToWorkSpaceDict
+        {
+            get { return _workspaceIdDict; }
+        }
+
+        public string ID { get { return "MAIN_ID"; } }
+        public override void Delete() { }
+    }
+}
---
+++ NuSysApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class MainViewModel : AtomViewModel
+    {
+        public MainViewModel(MainModel model) : base(model, null)
+        {
+            Model = model;
+        }
+
+        public MainModel Model { get; set; }
+
+        public override void Remove() { }
+
+        public override void UpdateAnchor() { }
+    }
+}
