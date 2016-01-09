23235029bc2589b3cc17d16fdea59037ea611409
Trent Green <trent_green@brown.edu>
2016-01-08 19:07:20 -0500

    added in null check for imagenodemodel unpack function

--- NuSysApp/Models/ImageNodeModel.cs
+++ NuSysApp/Models/ImageNodeModel.cs
@@ -37,8 +37,14 @@ namespace NuSysApp
        
         public override async Task UnPack(Message props)
         {
-            var data = Convert.FromBase64String(SessionController.Instance.ContentController.Get(props.GetString("contentId", null)).Data); //Converts to Byte Array
-            Image = await ByteArrayToBitmapImage(data);
+            if (props.ContainsKey("data"))
+            {
+                var data =
+                    Convert.FromBase64String(
+                        SessionController.Instance.ContentController.Get(props.GetString("contentId", null)).Data);
+                    //Converts to Byte Array
+                Image = await ByteArrayToBitmapImage(data);
+            }
             FilePath = props.GetString("filepath", FilePath);
             await base.UnPack(props);
         }
