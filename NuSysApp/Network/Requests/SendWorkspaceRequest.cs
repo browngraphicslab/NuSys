29ca544932245584ddff692e9305f036935cc8c2
Trent Green <trent_green@brown.edu>
2016-01-10 16:36:20 -0500

    forgot to add file

---
+++ NuSysApp/Network/Requests/SendWorkspaceRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace NuSysApp.Network.Requests
+{
+    public class SendWorkspaceRequest : SystemRequest
+    {
+        public SendWorkspaceRequest() : base(SystemRequestType.SendWorkspace)
+        {
+        }
+        public override async Task CheckOutgoingRequest()
+        {
+            await Task.Run(async delegate
+            {
+                var file = await StorageUtil.CreateFileIfNotExists(NuSysStorages.SaveFolder, "workspace.nusys");
+                _message["nodelines"] = new List<string>(await FileIO.ReadLinesAsync(file));
+            });
+        }
+
+        public virtual async Task ExecuteSystemRequestFunction(NuSysNetworkSession nusysSession, NetworkSession session,
+            string senderIP)
+        {
+            await Task.Run(async delegate
+            {
+                var file = await StorageUtil.CreateFileIfNotExists(NuSysStorages.SaveFolder, "workspace.nusys");
+                var lines = _message.GetList<string>("nodelines");
+                await FileIO.WriteLinesAsync(file, lines);
+                await SessionController.Instance.LoadWorkspace();
+            });
+        }
+
+    }
+}
