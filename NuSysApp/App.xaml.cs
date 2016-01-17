d887db05e84d87a59d73ee38e09afc7ec1fb13cf
jessicafu <jessica_fu@brown.edu>
2016-01-17 12:21:26 -0500

    changed setup folder for nusys to localfolder

--- NuSysApp/App.xaml.cs
+++ NuSysApp/App.xaml.cs
@@ -88,8 +88,10 @@ namespace NuSysApp
 
         private static async Task<bool> SetupDirectories()
         {
-            var workspaceFolder = await AccessList.GetWorkspaceFolder();
-            NuSysStorages.NuSysTempFolder = await StorageUtil.CreateFolderIfNotExists(workspaceFolder, Constants.FolderNusysTemp);
+            //StorageFolder workspaceFolder = await AccessList.GetWorkspaceFolder();
+            StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            NuSysStorages.NuSysTempFolder = await StorageUtil.CreateFolderIfNotExists(localFolder, Constants.FolderNusysTemp);
             NuSysStorages.SaveFolder = await StorageUtil.CreateFolderIfNotExists(NuSysStorages.NuSysTempFolder, Constants.FolderSave);
             NuSysStorages.Thumbs = await StorageUtil.CreateFolderIfNotExists(NuSysStorages.SaveFolder, Constants.FolderThumbs);
             NuSysStorages.ChromeTransferFolder = await StorageUtil.CreateFolderIfNotExists(NuSysStorages.NuSysTempFolder, Constants.FolderChromeTransferName);
