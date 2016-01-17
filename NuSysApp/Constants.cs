7df5d97f33d63786a58060dc844e59000bec8673
Trent Green <trent_green@brown.edu>
2016-01-17 14:56:58 -0500

    added importing of audio/video files

--- NuSysApp/Constants.cs
+++ NuSysApp/Constants.cs
@@ -74,8 +74,9 @@ namespace NuSysApp
         public static IEnumerable<string> ImageFileTypes = new List<string> { ".bmp", ".png", ".jpeg", ".jpg", ".tif", ".tiff" };
 
         public static IEnumerable<string> PdfFileTypes   = new List<string> { ".pdf" };
-        public static IEnumerable<string> VideoFileTypes   = new List<string> { };//TODO add more types
-        public static IEnumerable<string> AllFileTypes   = VideoFileTypes.Concat(ImageFileTypes.Concat(PdfFileTypes));
+        public static IEnumerable<string> VideoFileTypes   = new List<string> {".mp4" };//TODO add more types
+        public static IEnumerable<string> AudioFileTypes = new List<string> {".mp3"};
+        public static IEnumerable<string> AllFileTypes   = VideoFileTypes.Concat(ImageFileTypes.Concat(PdfFileTypes.Concat(AudioFileTypes)));
 
         #region Cortana
         //public static IEnumerable<string> SpeechCommands = new List<string> { "open document", "create text", "create ink" };
