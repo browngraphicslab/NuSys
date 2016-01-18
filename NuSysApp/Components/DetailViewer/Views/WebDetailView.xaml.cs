e99fe2f13e7f93c5a2ff86069b9512bacc068dbe
Harsh Chandra <Harsh Chandra>
2016-01-18 18:50:22 -0500

    Browser node now updates URL between workspace and detailed view

--- NuSysApp/Components/DetailViewer/Views/WebDetailView.xaml.cs
+++ NuSysApp/Components/DetailViewer/Views/WebDetailView.xaml.cs
@@ -20,10 +20,12 @@ namespace NuSysApp
 {
     public sealed partial class WebDetailView : UserControl
     {
+        private WebNodeViewModel viewmodel;
         public WebDetailView(WebNodeViewModel vm)
         {
             InitializeComponent();
             DataContext = vm;
+            viewmodel = vm;
 
             Loaded += delegate(object sender, RoutedEventArgs args)
             {
@@ -38,7 +40,8 @@ namespace NuSysApp
                 xScrollViewer.Width = xWebView.Width / ratio;
                 xScrollViewer.Height = xWebView.Height / ratio;
 
-                (vm.Model as WebNodeModel).Url = "http://www.google.com";
+                //(vm.Model as WebNodeModel).Url = "http://www.google.com";
+                (vm.Model as WebNodeModel).Url = vm.Url;
             };
 
             (vm.Model as WebNodeModel).UrlChanged += OnUrlChanged;
@@ -54,8 +57,80 @@ namespace NuSysApp
             if (e.Key == VirtualKey.Enter)
             {
                 var vm = (WebNodeViewModel)DataContext;
-                (vm.Model as WebNodeModel).Url = xUrlBox.Text;
+                //(vm.Model as WebNodeModel).Url = xUrlBox.Text;
+                xWebView.Navigate(new Uri(this.checkIfUrlRight(xUrlBox.Text)));
+                //(vm.Model as WebNodeModel).Url = this.checkIfUrlRight(xUrlBox.Text);
             }
         }
+
+        private string checkIfUrlRight(string s)
+        {
+            string url = null;
+            string searchterms = null;
+
+            if (Uri.IsWellFormedUriString(s, UriKind.Absolute))
+            {
+                url = s;
+                return url;
+            }
+            else
+            {
+                if (s.StartsWith("http://") == false && s.EndsWith(".com") == true)
+                {
+                    if (!s.Contains("www."))
+                    {
+                        s = "http://www." + s;
+                        url = s;
+                    }
+                    else
+                    {
+                        s = "http://" + s;
+                        url = s;
+                    }
+
+                    return url;
+                }
+                else if (!s.EndsWith(".com"))
+                {
+                    List<string> terms = new List<string>();
+                    string[] separators = new string[] { ",", ".", "!", "\'", " ", "\'s" };
+                    foreach (string word in s.Split(separators, StringSplitOptions.RemoveEmptyEntries))
+                        terms.Add(word);
+                    foreach (string word in terms)
+                        searchterms = searchterms + word + "+";
+                    if (!searchterms.EndsWith("+"))
+                    {
+                        searchterms = searchterms.Remove(-1);
+                    }
+                    url = "http://www.google.com/search?q=" + searchterms;
+                    return url;
+                }
+                else
+                {
+                    url = s;
+                    return url;
+                }
+            }
+
+
+        }
+
+        private void XWebView_OnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            string url = sender.Source.AbsoluteUri;
+            xUrlBox.Text = url;
+        }
+
+        private void XWebView_OnNavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            string url = sender.Source.AbsoluteUri;
+            xUrlBox.Text = url;
+            if ((viewmodel.Model as WebNodeModel).Url != url)
+            {
+                (viewmodel.Model as WebNodeModel).Url = url;
+            }
+
+        }
+
     }
 }
