e0262c71666fa8ae51a76d4d48f3a7725aed529e
Harsh Chandra <harsh_chandra@brown.edu>
2016-02-06 16:44:22 -0500

    List view just about functional

--- NuSysApp/Components/Misc/LibraryGrid.xaml.cs
+++ NuSysApp/Components/Misc/LibraryGrid.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -15,13 +16,44 @@ using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace NuSysApp.Components.Misc
+namespace NuSysApp
 {
     public sealed partial class LibraryGrid : UserControl
     {
-        public LibraryGrid()
+        public ObservableCollection<LibraryElement> _items; 
+        public LibraryGrid(ObservableCollection<LibraryElement> items)
         {
             this.InitializeComponent();
+            _items = items;
         }
+
+        //public void addItems()
+        //{
+        //    foreach (LibraryElement item in _items)
+        //    {
+        //        Border wrapping = new Border();
+        //        wrapping.Padding = new Thickness(10);
+               
+        //        wrapping.Child = ;
+
+        //    }
+
+        //    int count = 0;
+        //    int numRows = 5;
+        //    int numCols = 5;
+        //    List<FrameworkElement> children = new List<FrameworkElement>();
+
+        //    for (int i = 0; i < numRows; i++)
+        //    {
+        //        for (int j = 0; j < numCols; j++)
+        //        {
+        //            var wrapping = children[count];
+        //            Grid.SetRow(wrapping, i);
+        //            Grid.SetColumn(wrapping, j);
+        //            xGrid.Children.Add(wrapping);
+        //            count++;
+        //        }
+        //    }
+        //}
     }
 }
