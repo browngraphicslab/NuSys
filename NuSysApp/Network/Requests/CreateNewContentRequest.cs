cc6c55509e1f10b3532d82784ccede85d56c2dfc
Trent Green <trent_green@brown.edu>
2016-02-19 16:55:54 -0500

    added request

---
+++ NuSysApp/Network/Requests/CreateNewContentRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class CreateNewContentRequest : Request
+    {
+        public CreateNewContentRequest(Message m) : base(RequestType.CreateNewContentRequest, m)
+        {
+            SetServerSettings();
+        }
+
+        public CreateNewContentRequest(string id, string data, string type = null, string title = null)
+            : base(RequestType.CreateNewContentRequest)
+        {
+            _message["id"] = id;
+            _message["data"] = data;
+            if (type != null)
+            {
+                _message["type"] = type;
+            }
+            if (title != null)
+            {
+                _message["title"] = title;
+            }
+            SetServerSettings();
+        }
+
+        private void SetServerSettings()
+        {
+            SetServerEchoType(ServerEchoType.None);
+            SetServerItemType(ServerItemType.Content);
+            SetServerRequestType(ServerRequestType.Add);
+        }
+    }
+}
