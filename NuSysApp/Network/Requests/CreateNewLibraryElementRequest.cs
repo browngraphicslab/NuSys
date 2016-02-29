f48631795ed28ad1a35ddd43df97c31937f7ce84
Trent Green <trent_green@brown.edu>
2016-02-28 19:14:12 -0500

    started working on server changes to recent merge

---
+++ NuSysApp/Network/Requests/CreateNewLibraryElementRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class CreateNewLibraryElementRequest : Request
+    {
+        public CreateNewLibraryElementRequest(Message m) : base(RequestType.CreateNewLibrayElementRequest, m)
+        {
+            SetServerSettings();
+        }
+
+        public CreateNewLibraryElementRequest(string id, string data, string type = null, string title = null)
+            : base(RequestType.CreateNewLibrayElementRequest)
+        {
+            _message["id"] = id;
+            _message["data"] = data;
+            if (type != null)
+            {
+                _message["type"] = type;
+            }
+            if (title != null)
+            {
+                _message["title"] = title;
+            }
+            SetServerSettings();
+        }
+
+        private void SetServerSettings()
+        {
+            SetServerEchoType(ServerEchoType.None);
+            SetServerItemType(ServerItemType.Content);
+            SetServerRequestType(ServerRequestType.Add);
+        }
+    }
+}
