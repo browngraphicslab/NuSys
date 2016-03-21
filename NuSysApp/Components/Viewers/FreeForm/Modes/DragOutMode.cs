1fc4ddf3ada4484eb1457305837e4c36c9e633b6
nickturley <nicholas_turley@brown.edu>
2016-03-21 13:34:32 -0400

    fixed dragout bugs

--- NuSysApp/Components/Viewers/FreeForm/Modes/DragOutMode.cs
+++ NuSysApp/Components/Viewers/FreeForm/Modes/DragOutMode.cs
@@ -19,20 +19,13 @@ namespace NuSysApp
         private FrameworkElement _dragItem;
         private ElementType _elementType;
         private DispatcherTimer _timer;
+        private int _counter = 0;
+        private readonly int _waitTime = 500;
 
-
-        public DragOutMode(FrameworkElement view) : base(view)
-        {
-
-        }
+        public DragOutMode(FrameworkElement view) : base(view){ }
 
         public override async Task Activate()
         {
-            //_view.ManipulationMode = ManipulationModes.All;
-            //_view.ManipulationDelta += OnManipulationDelta;
-            //_view.ManipulationStarted += OnManipulationStarted;
-            //_view.ManipulationStarting += OnManipulationStarting;
-
             FreeFormViewerViewModel wvm = (FreeFormViewerViewModel)_view.DataContext;
 
             foreach (var n in wvm.AtomViewList.Where(s => s.DataContext is ElementViewModel))
@@ -42,8 +35,6 @@ namespace NuSysApp
                 {
                     userControl.ManipulationMode = ManipulationModes.All;
                     userControl.ManipulationDelta += OnManipulationDelta;
-                    userControl.ManipulationStarting += OnManipulationStarting;
-                    userControl.ManipulationStarted += OnManipulationStarted;
                     userControl.PointerReleased += UserControlOnPointerReleased;
                 }
             }
@@ -51,170 +42,47 @@ namespace NuSysApp
             wvm.AtomViewList.CollectionChanged += AtomViewListOnCollectionChanged;
         }
 
+        public override async Task Deactivate()
+        {
+            FreeFormViewerViewModel wvm = (FreeFormViewerViewModel)_view.DataContext;
+
+            foreach (var n in wvm.AtomViewList.Where(s => s.DataContext is ElementViewModel))
+            {
+                var userControl = (UserControl)n;
+                if (userControl.DataContext is ElementViewModel)
+                {
+                    
+                    userControl.ManipulationDelta -= OnManipulationDelta;
+                    userControl.PointerReleased -= UserControlOnPointerReleased;
+                }
+            }
+
+            wvm.AtomViewList.CollectionChanged -= AtomViewListOnCollectionChanged;
+        }
+
+
+        //HANDLER METHODS START HERE
         private void UserControlOnPointerReleased(object sender, PointerRoutedEventArgs pointerRoutedEventArgs)
         {
             _dragItem = null;
             _counter = 0;
-            if (_timer != null)
-            {
-                _timer.Stop();
-            }
+            _timer?.Stop();
             _timer = null;
         }
 
-
-        public override Task Deactivate()
-        {
-            throw new NotImplementedException();
-        }
-
+       
         private async void OnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs args)
         {
-            var cview = (AreaNodeView)_view;
-            var vm = (AreaNodeViewModel)cview.DataContext;
-            var send = (FrameworkElement)sender;
-            var sendVm = (ElementViewModel)send.DataContext;
-            var model = sendVm.Model;
-            var groupModel = vm.Model;
-
-            var point = vm.CompositeTransform.TransformPoint(args.Position);
-            var x = point.X + groupModel.X;
-            var y = point.Y + groupModel.Y; var point2 = SessionController.Instance.ActiveFreeFormViewer.CompositeTransform.TransformPoint(new Point(x, y));
-            var hits = VisualTreeHelper.FindElementsInHostCoordinates(new Point(x, y), SessionController.Instance.SessionView);
-            var result = hits.Where((uiElem) =>
+            if (!IsPointerInGroup(sender, args))
             {
-                var fe = (FrameworkElement)uiElem;
-                var r = fe is AreaNodeView;
-                return r;
-            });
-            if (!result.Any())
-            {
-
-                if (_timer == null)
-                {
-                    
-                    _timer = new DispatcherTimer();
-                    _timer.Tick += async delegate (object o, object o1)
-                    {
-                        _counter++;
-                        if (_counter == 1)
-                        {
-                            
-                      
-                            var bmp = new RenderTargetBitmap();
-                            await bmp.RenderAsync((UIElement)sender);
-                            var img = new Image
-                            {
-                                Opacity = 30,
-                                RenderTransform = new CompositeTransform(),
-                                Source = bmp
-                            };
-
-                            _dragItem = img;
-                            //cview.OuterCanvas.Children.Add(_dragItem);
-                            SessionController.Instance.SessionView.MainCanvas.Children.Add(_dragItem);
-                            this.SetUpObject(sender);
-                            var t = (CompositeTransform)_dragItem.RenderTransform;
-                            t.TranslateX += args.Delta.Translation.X;
-                            t.TranslateY += args.Delta.Translation.Y;
-
-                        sendVm.IsVisible = false;
-                        }
-                        if (_counter == 2)
-                        {
-                            var controller = SessionController.Instance.IdToControllers[model.Id];
-                            await controller.RequestMoveToCollection(WaitingRoomView.InitialWorkspaceId);
-                            _timer.Stop();
-                            _counter = 0;
-                        }
-                        
-                        
-                        
-                    };
-                    _timer.Interval = TimeSpan.FromMilliseconds(500);
-                    _timer.Start();
-
-                }
-
-
-
-
-               
+                this.StartTimer(sender, args);
             }
             else
             {
-                return;
+                this.StopTimer(sender, args);
             }
-
-
-         
-            //var d = (AreaNodeViewModel) (cview.DataContext);
-            ////rect.Intersect(d.ClipRect);
-            //if (rect.IsEmpty)
-            //{
-            //    //START TIMER
-            //    int a = 0;
-            //}
-            //var a = VisualTreeHelper.  var cview = (AreaNodeView)_view;
-
-
-
-
-
-
-
         }
 
-        private int _counter = 0;
-        private void SetUpObject(object sender)
-        {
-            if (_dragItem == null)
-                return;
-            _dragItem.Opacity = 0.5;
-            var cview = (AreaNodeView)_view;
-            var vm = (AreaNodeViewModel)cview.DataContext;
-            var send = (FrameworkElement)sender;
-            var sendVm = (ElementViewModel)send.DataContext;
-            var model = sendVm.Model;
-            var groupModel = vm.Model;
-            var point = vm.CompositeTransform.TransformPoint(new Point(model.X, model.Y));
-            var x = point.X + groupModel.X;
-            var y = point.Y + groupModel.Y;
-            var point2 = SessionController.Instance.ActiveFreeFormViewer.CompositeTransform.TransformPoint(new Point(x, y));
-            var t = (CompositeTransform)_dragItem.RenderTransform;
-            t.TranslateX = point2.X;
-            t.TranslateY = point2.Y;
-        }
-
-        private void OnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs args)
-        {
-            //if (_dragItem == null)
-            //    return;
-            //_dragItem.Opacity = 0.5;
-            //var cview = (AreaNodeView)_view;
-            //var vm = (AreaNodeViewModel) cview.DataContext;
-            //var send = (FrameworkElement) sender;
-            //var sendVm = (ElementViewModel)send.DataContext;
-            //var model = sendVm.Model;
-            //var groupModel = vm.Model;
-            //var point = vm.CompositeTransform.TransformPoint(new Point(model.X, model.Y));
-            //var x = point.X + groupModel.X;
-            //var y = point.Y + groupModel.Y;
-            //var point2 = SessionController.Instance.ActiveFreeFormViewer.CompositeTransform.TransformPoint(new Point(x, y));
-            //var t = (CompositeTransform) _dragItem.RenderTransform;
-            //t.TranslateX = point2.X;
-            //t.TranslateY = point2.Y;
-            
-          
-        }
-   
-        private async void OnManipulationStarting(object sender, ManipulationStartingRoutedEventArgs args)
-        {
-            //_elementType = sender == btnAddNode ? ElementType.Text : ElementType.Image;
-           
-
-            //args.Handled = true;
-        }
 
         private void AtomViewListOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
         {
@@ -230,13 +98,115 @@ namespace NuSysApp
                 {
                     userControl.ManipulationMode = ManipulationModes.All;
                     userControl.ManipulationDelta += OnManipulationDelta;
-                    userControl.ManipulationStarting += OnManipulationStarting;
-                    userControl.ManipulationStarted += OnManipulationStarted;
                     userControl.PointerReleased += UserControlOnPointerReleased;
                 }
             }
         }
 
+        //PRIVATE HELPER METHODS START HERE
+        private void SetUpDragObject(object sender)
+        {
+            if (_dragItem == null)
+                return;
+            var cview = (AreaNodeView)_view;
+            var vm = (AreaNodeViewModel)cview.DataContext;
+            var send = (FrameworkElement)sender;
+            var sendVm = (ElementViewModel)send.DataContext;
+            var model = sendVm.Model;
+            var groupModel = vm.Model;
+            var point = vm.CompositeTransform.TransformPoint(new Point(model.X, model.Y));
+            var x = point.X + groupModel.X;
+            var y = point.Y + groupModel.Y;
+            var point2 = SessionController.Instance.ActiveFreeFormViewer.CompositeTransform.TransformPoint(new Point(x, y));
+            var t = (CompositeTransform)_dragItem.RenderTransform;
+            t.TranslateX = point2.X;
+            t.TranslateY = point2.Y;
+        }
 
+        private Point GetRealCoordinatesOnScreen(object sender, ManipulationDeltaRoutedEventArgs args)
+        {
+            var cview = (AreaNodeView)_view;
+            var vm = (AreaNodeViewModel)cview.DataContext;
+            var send = (FrameworkElement)sender;
+            var sendVm = (ElementViewModel)send.DataContext;
+            var model = sendVm.Model;
+            var groupModel = vm.Model;
+
+            var point = vm.CompositeTransform.TransformPoint(args.Position);
+            var x = point.X + groupModel.X;
+            var y = point.Y + groupModel.Y; var point2 = SessionController.Instance.ActiveFreeFormViewer.CompositeTransform.TransformPoint(new Point(x, y));
+            return new Point(x, y);
+        }
+
+        private bool IsPointerInGroup(object sender, ManipulationDeltaRoutedEventArgs args)
+        {
+            var hits = VisualTreeHelper.FindElementsInHostCoordinates(this.GetRealCoordinatesOnScreen(sender, args), SessionController.Instance.SessionView);
+            var result = hits.Where((uiElem) => uiElem is AreaNodeView);
+            return result.Any();
+        }
+
+        private void StartTimer(object sender, ManipulationDeltaRoutedEventArgs args)
+        {
+            var send = (FrameworkElement)sender;
+            var sendVm = (ElementViewModel)send.DataContext;
+            var model = sendVm.Model;
+            if (_timer == null)
+            {
+                _timer = new DispatcherTimer();
+                _timer.Tick += async delegate (object o, object o1)
+                {
+                    _counter++;
+                    if (_counter == 1)//This happens after dragging a node out and waiting 0.5 seconds 
+                    {
+                        var bmp = new RenderTargetBitmap();
+                        await bmp.RenderAsync((UIElement)sender);
+                        var img = new Image
+                        {
+                            RenderTransform = new CompositeTransform(),
+                            Source = bmp
+                        };
+                        _dragItem = img;
+                        SessionController.Instance.SessionView.MainCanvas.Children.Add(_dragItem);
+                        this.SetUpDragObject(sender);
+                        var t = (CompositeTransform)_dragItem.RenderTransform;
+                        t.TranslateX += args.Delta.Translation.X;
+                        t.TranslateY += args.Delta.Translation.Y;
+
+                        sendVm.IsVisible = false;
+                    }
+                    if (_counter == 2) //This happens after waiting another 0.5 seconds
+                    {
+                        var controller = SessionController.Instance.IdToControllers[model.Id];
+                        await controller.RequestMoveToCollection(WaitingRoomView.InitialWorkspaceId);
+                        this.StopTimer(sender, args);
+                    }
+                };
+                _timer.Interval = TimeSpan.FromMilliseconds(_waitTime);
+                _timer.Start();
+
+            }
+        }
+
+        private void StopTimer(object sender, ManipulationDeltaRoutedEventArgs args)
+        {
+            if (_dragItem != null)
+            {
+                SessionController.Instance.SessionView.MainCanvas.Children.Remove(_dragItem);
+            }
+            if (sender != null)
+            {
+                var send = (FrameworkElement)sender;
+                if (send != null)
+                {
+                    var sendVm = (ElementViewModel)send.DataContext;
+                    sendVm.IsVisible = true;
+                }
+                
+            }
+            _dragItem = null;
+            _counter = 0;
+            _timer?.Stop();
+            _timer = null;
+        }
     }
 }
