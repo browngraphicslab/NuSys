22a12ebd4b2259c4c3c7be95992aee84472a149a
Sol Kim <sol_kim@brown.edu>
2016-04-10 19:32:48 -0400

    new color scheme

--- NuSysApp/Constants.cs
+++ NuSysApp/Constants.cs
@@ -50,6 +50,7 @@ namespace NuSysApp
         #endregion Ink Node
 
         #region Color
+        /*
         public static Color color1 = Color.FromArgb(255, 230, 237, 236);   // lightest blue
         public static Color color2 = Color.FromArgb(255, 156, 197, 194);   // medium blue
         public static Color color3 = Color.FromArgb(255, 131, 166, 163);   // darkest blue
@@ -60,6 +61,19 @@ namespace NuSysApp
         public static Color color7 = Colors.LightGray;                     // light gray
         public static Color color8 = Colors.DarkGray;                      // dark gray
         public static Color color9 = Colors.DarkSalmon;                    // dark salmon
+        */
+
+        public static Color color1 = Color.FromArgb(255, 219, 231, 254);   // lightest blue
+        public static Color color2 = Color.FromArgb(255, 122, 137, 163);   // medium blue
+        public static Color color3 = Color.FromArgb(255, 71, 96, 122);     // darkest blue
+        public static Color color4 = Color.FromArgb(255, 211, 105, 120);   // red
+        public static Color color5 = Color.FromArgb(255, 143, 152, 161);   // dark gray blue
+        public static Color color6 = Colors.White;                         // white
+        public static Color foreground6 = Colors.Black;                    // foreground white
+        public static Color color7 = Color.FromArgb(255, 208, 208, 212);   // light gray
+        public static Color color8 = Colors.DarkGray;                      // dark gray
+        public static Color color9 = Colors.DarkSalmon;                    // dark salmon
+
         #endregion Color
 
         #region AtomType
