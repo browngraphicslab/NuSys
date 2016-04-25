ff73b9067bda9afdcd2cb24b327f5302e9d21b74
Philipp Eichmann <philipp.eichmann@gmail.com>
2016-04-25 11:17:00 -0400

    latest

--- NuSysApp/Network/Requests/DeleteLibraryElementRequest.cs
+++ NuSysApp/Network/Requests/DeleteLibraryElementRequest.cs
@@ -35,6 +35,8 @@ namespace NuSysApp
         public override async Task ExecuteRequestFunction()
         {
             var libraryElement = SessionController.Instance.ContentController.Get(_message.GetString("id"));
+            if (libraryElement == null)
+                return;
             libraryElement.FireDelete();
         }
     }
