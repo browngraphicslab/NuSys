dfb18c69e61074e5e93f03acf76dbeed39b370ef
trent <trent_green@brown.edu>
2016-06-10 14:41:27 -0400

    regions yo

---
+++ NuSysApp/Util/Regions/CompoundRegion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp.Util.Regions
+{
+    class CompoundRegion : Region
+    {
+        public CompoundRegion(params Region[] regions)
+        {
+            Regions = regions;
+        }
+        public Region[] Regions { set; get; }
+    }
+}
---
+++ NuSysApp/Util/Regions/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace NuSysApp
+{
+    abstract class Region
+    {
+        protected Region()
+        {
+        }
+    }
+}
