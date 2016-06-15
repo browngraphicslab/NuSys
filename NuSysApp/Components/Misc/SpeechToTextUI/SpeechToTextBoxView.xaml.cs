35169cc61cc1f65b9dc291fe9b4bff40a1543075
Luke Murray <luke_murray@brown.edu>
2016-06-15 14:59:17 -0400

    Created simple view and view model hierarchy in SpeechToTextUI hierarchy with button and textbox

---
+++ NuSysApp/Components/Misc/SpeechToTextUI/SpeechToTextBoxView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NuSysApp.Components.Misc.SpeechToTextUI
+{
+    public sealed partial class SpeechToTextBoxView : UserControl
+    {
+
+        SpeechToTextBoxViewModel _vm = new SpeechToTextBoxViewModel();
+
+        public SpeechToTextBoxView()
+        {
+            this.InitializeComponent();
+        }
+
+        // used for dragging the grid around the screen
+        private void RootGrid_OnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            xMatrixTransform.Matrix = xTransformGroup.Value;
+            xCompositeTransform.TranslateX = e.Delta.Translation.X;
+            xCompositeTransform.TranslateY = e.Delta.Translation.Y;
+            e.Handled = true;
+        }
+    }
+}
---
+++ NuSysApp/Components/Misc/SpeechToTextUI/SpeechToTextBoxViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NuSysApp.Annotations;
+
+namespace NuSysApp.Components.Misc.SpeechToTextUI
+{
+    class SpeechToTextBoxViewModel : INotifyPropertyChanged
+    {
+
+        private string _text;
+        internal string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                OnPropertyChanged();
+                Debug.WriteLine(_text);
+            }
+        }
+
+
+
+        public SpeechToTextBoxViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
