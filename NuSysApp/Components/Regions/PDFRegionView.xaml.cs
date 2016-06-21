b6d4ca7e9f33696e50f00c78ac096095bae867de
Harsh Chandra <harsh_chandra@brown.edu>
2016-06-21 16:05:32 -0400

    Region controller added

--- NuSysApp/Components/Regions/PDFRegionView.xaml.cs
+++ NuSysApp/Components/Regions/PDFRegionView.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Foundation;
 using System.ComponentModel;
+using System.Diagnostics;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -23,6 +24,8 @@ namespace NuSysApp
     { 
         public Point _topLeft;
         public Point _bottomRight;
+        private double _tx;
+        private double _ty;
 
         public delegate void RegionSelectedEventHandler(object sender, bool selected);
         public event RegionSelectedEventHandler OnSelected;
@@ -34,7 +37,6 @@ namespace NuSysApp
             this.DataContext = regionVM;
             this.Selected();
           
-            regionVM.PropertyChanged += RegionVM_PropertyChanged;
             regionVM.RegionChanged += RegionVM_RegionChanged;
             OnSelected?.Invoke(this, true);
             
@@ -53,42 +55,27 @@ namespace NuSysApp
             var parentHeight = regionVM.ContainerViewModel.GetHeight();
             composite.TranslateX = model.TopLeftPoint.X * parentWidth;
             composite.TranslateY = model.TopLeftPoint.Y * parentHeight;
-            xMainRectangle.Width = (model.BottomRightPoint.X - model.TopLeftPoint.X) * parentWidth;
-            xMainRectangle.Height = (model.BottomRightPoint.Y - model.TopLeftPoint.Y) * parentHeight;
+            _tx = composite.TranslateX;
+            _ty = composite.TranslateY;
+            regionVM.Width = (model.BottomRightPoint.X - model.TopLeftPoint.X) * parentWidth;
+            regionVM.Height = (model.BottomRightPoint.Y - model.TopLeftPoint.Y) * parentHeight;
 
         }
 
         private void RegionVM_RegionChanged(object sender, double height, double width)
         {
-            xMainRectangle.Width = Width;
-            xMainRectangle.Height = Height;
+            return;
+            var vm = (PdfRegionViewModel) DataContext;
+            Debug.WriteLine("oooooooooooooo");
+            vm.Width = width;
+            vm.Height = height;
         }
 
-        private void RegionVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            var vm = DataContext as PdfRegionViewModel;
-            switch (e.PropertyName)
-            {
-                case "Width":
-                    if (vm == null)
-                    {
-                        break;
-                    }
-                    xMainRectangle.Width = vm.Width;
-                    break;
-
-                case "Height":
-                    if (vm == null)
-                    {
-                        break;
-                    }
-                    xMainRectangle.Height = vm.Height;
-                    break;
-            }
-        }
+        
 
         private void ChangeSize(object sender, Point topLeft, Point bottomRight)
         {
+            return;
             var composite = RenderTransform as CompositeTransform;
             if (composite == null)
             {
@@ -96,6 +83,8 @@ namespace NuSysApp
             }
             composite.TranslateX = topLeft.X;
             composite.TranslateY = topLeft.Y;
+           // _tx = composite.TranslateX;
+           // _ty = composite.TranslateY;
         }
         private void UpdateViewModel()
         {
@@ -112,15 +101,16 @@ namespace NuSysApp
 
         private void XResizingRectangle_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
-
-            xMainRectangle.Width += e.Delta.Translation.X;
-            xMainRectangle.Height += e.Delta.Translation.Y;
+//            xMainRectangle.Width += e.Delta.Translation.X;
+ //           xMainRectangle.Height += e.Delta.Translation.Y;
             RectangleTranform.CenterX += e.Delta.Translation.X;
             RectangleTranform.CenterY += e.Delta.Translation.Y;
             xGrid.Width += e.Delta.Translation.X;
             xGrid.Height += e.Delta.Translation.Y;
             GridTranform.CenterX += e.Delta.Translation.X;
             GridTranform.CenterY += e.Delta.Translation.Y;
+            
+            
             //ResizerTransform.TranslateX += e.Delta.Translation.X;
             //ResizerTransform.TranslateY += e.Delta.Translation.Y;
 
@@ -137,22 +127,58 @@ namespace NuSysApp
 
         private void RectangleRegionView_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
-            if (((CompositeTransform) this.RenderTransform).TranslateX < 0)
+            var vm = DataContext as PdfRegionViewModel;
+            if (vm == null)
             {
-                //this.Width += e.Delta.Translation.X;
+                return;
+            }
+
+            var rt = ((CompositeTransform)this.RenderTransform);
+
+            _tx += e.Delta.Translation.X;
+            _ty += e.Delta.Translation.Y;
+
+            if (_tx < 0)
+            {
+                Debug.WriteLine(vm.OriginalWidth);
+                Debug.WriteLine(_tx);
+                Debug.WriteLine("-------");
+
+                vm.Width = vm.OriginalWidth + _tx;
+                rt.TranslateX = 0;
             }
             else
             {
-                ((CompositeTransform)this.RenderTransform).TranslateX += e.Delta.Translation.X;
-                ((CompositeTransform)this.RenderTransform).TranslateY += e.Delta.Translation.Y;
-                UpdateViewModel();
+                rt.TranslateX = _tx;
+                vm.Width = vm.OriginalWidth;
             }
             
+            
+      
+              //  ((CompositeTransform)this.RenderTransform).TranslateY += e.Delta.Translation.Y;
+            
+                
+            UpdateViewModel();
             e.Handled = true;
         }
 
         private void RectangleRegionView_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
         {
+            var vm = DataContext as PdfRegionViewModel;
+            if (vm == null)
+            {
+                return;
+            }
+            var tx = ((CompositeTransform) this.RenderTransform).TranslateX;
+            var ty = ((CompositeTransform) this.RenderTransform).TranslateY;
+            if (tx < 0 || tx + vm.Width > vm.ContainerWidth)
+                return;
+            if (ty < 0 || ty + vm.Height > vm.ContainerHeight)
+                return;
+
+
+            vm.OriginalHeight = vm.Height;
+            vm.OriginalWidth = vm.Width;
             OnSelected?.Invoke(this, true);
             e.Handled = true;
         }
