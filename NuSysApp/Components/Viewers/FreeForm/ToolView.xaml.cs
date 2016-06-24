52713dd17442a343cfd3fb712e0c3e3499872d89
lbengzon <leandro_bengzon@brown.edu>
2016-06-24 12:16:25 -0400

    push for nick

---
+++ NuSysApp/Components/Viewers/FreeForm/ToolView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NuSysApp.Components.Viewers.FreeForm
+{
+    public sealed partial class ToolView : UserControl
+    {
+        public ToolView()
+        {
+            this.InitializeComponent();
+
+            xFilterElement.AddHandler(PointerPressedEvent, new PointerEventHandler(BtnAddOnManipulationStarting), true);
+            xFilterElement.AddHandler(PointerReleasedEvent, new PointerEventHandler(BtnAddOnManipulationCompleted), true);
+        }
+    }
+}
