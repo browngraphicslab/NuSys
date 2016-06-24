c861efe1d56100ffd32ab1637dc03276c7ea95c4
Trent Green <Trent_green@brown.edu>
2016-06-24 15:32:50 -0400

    more changed to tools

--- NuSysApp/Components/Tools/ToolController.cs
+++ NuSysApp/Components/Tools/ToolController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyToolkit.Utilities;
 
 namespace NuSysApp
 {
@@ -32,6 +33,11 @@ namespace NuSysApp
             FilterChanged?.Invoke(this,filter);
         }
 
+        public void UnSelect()
+        {
+            Model.SetSelection(null);
+            SelectionChanged?.Invoke(this, null);
+        }
         public void SetSelection(string selection)
         {
             Model.SetSelection(selection);
@@ -71,6 +77,50 @@ namespace NuSysApp
             }
         }
 
+        public List<string> GetAllProperties()
+        {
+            var libraryElementControllers = Model.LibraryIds.Select( id => SessionController.Instance.ContentController.GetLibraryElementController(id));
+            var allMetadata = new Dictionary<string, List<string>>();
+            foreach (var controller in libraryElementControllers)
+            {
+                foreach (var kvp in GetMetadata(controller.LibraryElementModel.LibraryElementId))
+                {
+                    if (!allMetadata.ContainsKey(kvp.Key))
+                    {
+                        allMetadata.Add(kvp.Key, new List<string>());
+                    }
+                    allMetadata[kvp.Key].Add(kvp.Value);
+                }
+            }
+            switch (Model.Filter)
+            {
+                case ToolModel.FilterTitle.Creator:
+                    return allMetadata.ContainsKey("Creator") ? allMetadata["Creator"] : new List<string>();
+                    break;
+                case ToolModel.FilterTitle.Title:
+                    return allMetadata.ContainsKey("Title") ? allMetadata["Title"] : new List<string>();
+                    break;
+                case ToolModel.FilterTitle.Type:
+                    return allMetadata.ContainsKey("Type") ? allMetadata["Type"] : new List<string>();
+                    break;
+                case ToolModel.FilterTitle.Date:
+                    return allMetadata.ContainsKey("Date") ? allMetadata["Date"] : new List<string>();
+                    break;
+                case ToolModel.FilterTitle.MetadataKeys:
+                    return allMetadata.Keys.ToList();
+                    break;
+                case ToolModel.FilterTitle.MetadataValues:
+                    var ret = new List<string>();
+                    foreach (var values in allMetadata.Values)
+                    {
+                        ret.AddRange(values);
+                    }
+                    return ret;
+                    break;
+            }
+            return new List<string>();
+        }
+
         public void Dispose()
         {
             foreach(var parentController in Model.ParentIds.Select(id => ToolControllers.ContainsKey(id) ? ToolControllers[id] : null))
@@ -78,19 +128,86 @@ namespace NuSysApp
                 RemoveParent(parentController);
             }
         }
+
+        private HashSet<string> Filter(HashSet<string> ids)
+        {
+            if (Model.Selection == null)
+            {
+                return ids;
+            }
+            var ret = new HashSet<string>();
+            foreach (string id in ids)
+            {
+                if (IncludeInFilter(id, Model.Selection))
+                {
+                    ret.Add(id);
+                }
+            }
+            return ret;
+        }
+
+        private bool IncludeInFilter(string libraryId, string selection)
+        {
+            var libraryElementModel = SessionController.Instance.ContentController.GetContent(libraryId);
+            if (libraryElementModel == null)
+            {
+                return false;
+            }
+            switch (Model.Filter)
+            {
+                case ToolModel.FilterTitle.Title:
+                    return libraryElementModel.Title == selection;
+                    break;
+                case ToolModel.FilterTitle.Type:
+                    return libraryElementModel.Type.ToString().ToLower().Equals(selection.ToLower());
+                    break;
+                case ToolModel.FilterTitle.Creator:
+                    return libraryElementModel.Creator == selection;
+                    break;
+                case ToolModel.FilterTitle.Date:
+                    return GetDate(libraryElementModel) == selection;
+                    break;
+                case ToolModel.FilterTitle.MetadataKeys:
+                    return libraryElementModel.Metadata.ContainsKey(selection);
+                    break;
+                case ToolModel.FilterTitle.MetadataValues:
+                    return libraryElementModel.Metadata.Any(item => item.Value.Item1 == selection);
+                    break;
+            }
+            return false;
+        }
+
+        private string GetDate(LibraryElementModel libraryElementModel)
+        {
+            return DateTime.Parse(libraryElementModel.Timestamp).ToStartOfDay().ToString();
+        }
+        private Dictionary<string, string> GetMetadata(string libraryId)
+        {
+            var element = SessionController.Instance.ContentController.GetContent(libraryId);
+            if (element != null)
+            {
+                var metadata = element.Metadata.ToDictionary(k=>k.Key,v=>v.Value.Item1);
+                metadata["Title"] = element.Title;
+                metadata["Type"] = element.Type.ToString();
+                metadata["Date"] = GetDate(element);
+                metadata["Creator"] = element.Creator;
+                return metadata;
+            }
+            return new Dictionary<string, string>();
+        }
         private void ParentFilterChanged(object sender, ToolModel.FilterTitle filter)
         {
-            Model.SetLibraryIds(GetUpdatedDataList());
+            Model.SetLibraryIds(Filter(GetUpdatedDataList()));
             LibraryIdsChanged?.Invoke(this, Model.LibraryIds);
         }
         private void ParentSelectionChanged(object sender, string selection)
         {
-            Model.SetLibraryIds(GetUpdatedDataList());
+            Model.SetLibraryIds(Filter(GetUpdatedDataList()));
             LibraryIdsChanged?.Invoke(this, Model.LibraryIds);
         }
         private void ParentLibraryIdsChanged(object sender, HashSet<string> libraryIds)
         {
-            Model.SetLibraryIds(GetUpdatedDataList());
+            Model.SetLibraryIds(Filter(GetUpdatedDataList()));
             LibraryIdsChanged?.Invoke(this, Model.LibraryIds);
         }
         private HashSet<string> GetUpdatedDataList()
