afd2fed9efd5834bfc4bcab02b8e1752520f81b0
nicolas choi <nicolas_choi@brown.edu>
2016-06-24 14:05:16 -0400

    started link work

---
+++ NuSysApp/Components/Tools/ToolLinkView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NuSysApp
+{
+    public sealed partial class ToolLinkView : AnimatableUserControl
+    {
+        public ToolLinkView()
+        {
+            this.InitializeComponent();
+
+
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Annotation_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
