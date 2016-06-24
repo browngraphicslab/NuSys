b320138fed30a2d29a179890bd25b92adbb99c9c
Trent Green <Trent_green@brown.edu>
2016-06-24 10:59:15 -0400

    pushing tools

---
+++ NuSysApp/Components/Tools/ToolController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class ToolController
+    {
+        public delegate void FilterChangedEventHandler(object sender, ToolModel.FilterTitle filter);
+        public delegate void SelectionChangedEventHandler(object sender, string selection);
+        public delegate void LibraryIdsChangedEventHandler(object sender, List<string> libraryIds);
+
+        public event FilterChangedEventHandler FilterChanged;
+        public event SelectionChangedEventHandler SelectionChanged;
+        public event LibraryIdsChangedEventHandler LibraryIdsChanged;
+
+        public ToolModel Model { get; private set; }
+        public ToolController(ToolModel model)
+        {
+            Debug.Assert(model != null);
+            Model = model;
+        }
+
+        public void SetFilter(ToolModel.FilterTitle filter)
+        {
+            Model.SetFilter(filter);
+            FilterChanged?.Invoke(this,filter);
+        }
+
+        public void SetSelection(string selection)
+        {
+            Model.SetSelection(selection);
+
+        }
+
+    }
+}
---
+++ NuSysApp/Components/Tools/ToolModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class ToolModel
+    {
+        public enum FilterTitle
+        {
+            Title,
+            Type,
+            Creator,
+            Date,
+            MetadataKeys,
+            MetadataValues
+        }
+        public FilterTitle Filter { get; private set; }
+        public List<string> LibraryIds { get; private set; }
+        public string Selection { get; private set; }
+
+        public void SetFilter(FilterTitle filter)
+        {
+            Filter = filter;
+        }
+
+        public void SetLibraryIds(List<string> libraryIds)
+        {
+            LibraryIds = libraryIds;
+        }
+
+        public bool AddLibraryId(string libraryId)
+        {
+            if (LibraryIds.Contains(libraryId))
+            {
+                return false;
+            }
+            LibraryIds.Add(libraryId);
+            return true;
+        }
+        public bool RemoveLibraryId(string libraryId)
+        {
+            if (LibraryIds.Contains(libraryId))
+            {
+                LibraryIds.Remove(libraryId);
+                return true;
+            }
+            return false;
+        }
+
+        public void SetSelection(string selection)
+        {
+            Selection = selection;
+        }
+    }
+}
