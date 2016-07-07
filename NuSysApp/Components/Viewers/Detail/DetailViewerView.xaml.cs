0bccdb35f8686701c6ab0d11123177fdf096c259
nicolas choi <nicolas_choi@brown.edu>
2016-07-07 12:50:56 -0400

    videos and audio stop playing when dvv closed 2

--- NuSysApp/Components/Viewers/Detail/DetailViewerView.xaml.cs
+++ NuSysApp/Components/Viewers/Detail/DetailViewerView.xaml.cs
@@ -344,11 +344,25 @@ namespace NuSysApp
             var textview = (vm.View as TextDetailHomeTabView);
             textview?.Dispose();
             var videoView = vm.View as VideoDetailHomeTabView;
-            //not implemented
             videoView?.Dispose();
             videoView?.StopVideo();
+            if (vm.RegionView != null)
+            {
+                var videoRegionView = vm.RegionView as VideoDetailHomeTabView;
+                videoRegionView?.Dispose();
+                videoRegionView?.StopVideo();
+            }
+
             var Audioview = vm.View as AudioDetailHomeTabView;
+            Audioview?.Dispose();
             Audioview?.StopAudio();
+
+            if (vm.RegionView != null)
+            {
+                var audioRegionView = vm.RegionView as AudioDetailHomeTabView;
+                audioRegionView?.Dispose();
+                audioRegionView?.StopAudio();
+            }
             
 
         }
