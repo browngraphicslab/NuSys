3b038ab60f0a79bd67e757801f988f48dec8cf7d
Luke Murray <Luke_Murray@brown.edu>
2016-07-14 17:35:38 -0400

    removed deprecated code

--- NuSysApp/Components/SessionView.xaml.cs
+++ NuSysApp/Components/SessionView.xaml.cs
@@ -791,10 +791,6 @@ namespace NuSysApp
             }
             if (viewable is RegionController)
             {
-                //var libraryElementController =
-                //    SessionController.Instance.ContentController.GetLibraryElementController(
-                //        (viewable as RegionController).ContentId);
-                //await xDetailViewer.ShowElement(libraryElementController , tabToOpenTo);
                 await xDetailViewer.ShowElement(viewable as RegionController, tabToOpenTo);
 
             }
