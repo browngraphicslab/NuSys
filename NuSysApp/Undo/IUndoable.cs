1dbaf1dcdee18ee51717f24c832e10df7351b8a9
Zach Kirschenbaum <zachary_kirschenbaum@brown.edu>
2016-08-05 15:15:38 -0400

    added some classes

---
+++ NuSysApp/Undo/IUndoable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    /// <summary>
+    /// All undoable actions will implement this undoable interface
+    /// </summary>
+   public interface IUndoable
+   {
+        /// <summary>
+        /// Returns the inverse--an action representing the "undone" version of the current action
+        /// </summary>
+        /// <returns></returns>
+       IUndoable GetInverse();
+
+        /// <summary>
+        /// To extract the undoable action as a Request that you can use to regenerate the state of the action
+        /// </summary>
+        /// <returns></returns>
+       Request ToRequest();
+   }
+}
---
+++ NuSysApp/Undo/MoveElementAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    public class MoveElementAction : IUndoable
+    {
+        public IUndoable GetInverse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Request ToRequest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
