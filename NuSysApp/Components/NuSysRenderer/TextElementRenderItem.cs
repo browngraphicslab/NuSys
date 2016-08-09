849c9f198ede1b88ee3c94b69a098eb8e26d9fcb
Harsh Chandra <harsh_chandra@brown.edu>
2016-08-09 12:39:28 -0400

    Parser integrated into textelementrenderitem

--- NuSysApp/Components/NuSysRenderer/TextElementRenderItem.cs
+++ NuSysApp/Components/NuSysRenderer/TextElementRenderItem.cs
@@ -24,10 +24,12 @@ namespace NuSysApp
     public class TextElementRenderItem : ElementRenderItem
     {
         private TextNodeViewModel _vm;
+        private HTMLParser _htmlParser;
 
         public TextElementRenderItem(TextNodeViewModel vm, CollectionRenderItem parent, CanvasAnimatedControl resourceCreator):base(vm, parent, resourceCreator)
         {
             _vm = vm;
+            _htmlParser = new HTMLParser(resourceCreator);
         }
 
         public override void Dispose()
@@ -44,7 +46,11 @@ namespace NuSysApp
             ds.Transform = Win2dUtil.Invert(C) * S * C * T * ds.Transform;
 
             ds.FillRectangle( new Rect {X = 0, Y = 0, Width = _vm.Width, Height=_vm.Height}, Colors.White);
-            
+
+            var textLayout = _htmlParser.GetParsedText(_vm.Text, _vm.Height, _vm.Width);
+            textLayout.HorizontalAlignment = CanvasHorizontalAlignment.Center;
+            ds.DrawTextLayout(textLayout, (float)_vm.X, (float)_vm.Y, Colors.Black);
+            /*
             var f = new CanvasTextFormat();
             f.WordWrapping = CanvasWordWrapping.Wrap;
             f.FontSize = 10;
@@ -53,6 +59,7 @@ namespace NuSysApp
                 l.HorizontalAlignment = CanvasHorizontalAlignment.Center;
                 ds.DrawTextLayout(l, (float)_vm.X, (float)_vm.Y, Colors.Black);
             }
+            */
 
             ds.Transform = orgTransform;
 
