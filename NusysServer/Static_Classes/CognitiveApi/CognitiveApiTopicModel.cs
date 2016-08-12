a8936af37c965be8851fbc7b91af229fc711918b
Luke Murray <Luke Murray>
2016-08-12 14:21:43 -0400

    merged cognitive stuff

---
+++ NusysServer/Static_Classes/CognitiveApi/CognitiveApiTopicModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NusysServer
+{
+    /// <summary>
+    /// Models the return results for a cognitive services key phrases topic analysis
+    /// </summary>
+    public class CognitiveApiTopicModel
+    {
+        // All public properties are lowercase because they have to be serialized as lowercase in the cog services api
+
+        /// <summary>
+        /// The processing result, use this to get data from the model. Only exists if status = succeeded
+        /// </summary>
+        public CognitiveApiTopicProcessingResult operationProcessingResult { get; set; }
+
+        /// <summary>
+        /// The status of the operation. Takes 3 possible values
+        /// 
+        ///     notstarted  - the process has not started
+        ///     running     - the process is running
+        ///     succeeded   - the process has succeeded
+        /// </summary>
+        public string status { get; set; }
+
+        /// <summary>
+        /// The date time when the operation was started
+        /// </summary>
+        public string createdDateTime { get; set; }
+
+        /// <summary>
+        /// The type of the operation being processed
+        /// </summary>
+        public string operationType { get; set; }
+
+    }
+}
---
+++ NusysServer/Static_Classes/CognitiveApi/ICognitiveApiDocumentable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NusysServer
+{
+    /// <summary>
+    /// Interface for creating document models that can be sent to the Cognitive Services API for Text Analysis
+    /// </summary>
+    public interface ICognitiveApiDocumentable
+    {
+        /// <summary>
+        /// A unique identifier for the document, can only be one instance of each id in a congnitive services request
+        /// </summary>
+        string id { get; set; }
+
+        /// <summary>
+        /// The text contents of the document. 
+        /// </summary>
+        string text { get; set; }
+
+    }
+}
