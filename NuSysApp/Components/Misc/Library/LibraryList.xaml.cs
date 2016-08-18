744ed5cc1074850b1fec1676a0d65945557fd5a6
Trent Green <Trent_green@brown.edu>
2016-08-18 14:43:38 -0400

    merged and fixed

--- NuSysApp/Components/Misc/Library/LibraryList.xaml.cs
+++ NuSysApp/Components/Misc/Library/LibraryList.xaml.cs
@@ -478,10 +478,6 @@ namespace NuSysApp
             args.Favorited = model.Favorited;
             args.Keywords = model.Keywords;
             args.Title = model.Title;
-            args.Small_Thumbnail_Bytes = model.SmallIconUrl;
-            args.Medium_Thumbnail_Bytes = model.MediumIconUrl;
-            args.Large_Thumbnail_Bytes = model.LargeIconUrl;
-            args.Title = model.Title;
             //args.Creator
             args.LibraryElementType = model.Type;
             args.ContentId = model.ContentDataModelId;
