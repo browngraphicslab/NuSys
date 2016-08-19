dfdee4dc5d324e4a0281c3234ec4eab1d59685f0
lbengzon <leandro_bengzon@brown.edu>
2016-08-19 13:40:43 -0400

    request and handler for related documents

---
+++ NusysServer/RequestHandlers/GetRelatedDocumentsRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using NusysIntermediate;
+
+namespace NusysServer.RequestHandlers
+{
+    public class GetRelatedDocumentsRequestHandler: RequestHandler
+    {
+        public override Message HandleRequest(Request request, NuWebSocketHandler senderHandler)
+        {
+            Debug.Assert(request.GetRequestType() == NusysConstants.RequestType.GetRelatedDocumentsRequest);
+            var message = GetRequestMessage(request);
+            Debug.Assert(message.ContainsKey(NusysConstants.GET_RELATED_DOCUMENTS_REQUEST_LIBRARY_ID_KEY));
+            var libraryId = message.Get(NusysConstants.GET_RELATED_DOCUMENTS_REQUEST_LIBRARY_ID_KEY);
+            //TODO DO stuff with related documents and return the list of related documents
+            
+            var returnMessage = new Message(message);
+            //returnMessage[NusysConstants.REQUEST_SUCCESS_BOOL_KEY] = success;
+            //returnMessage[NusysConstants.GET_RELATED_DOCUMENTS_REQUEST_RETURNED_RELATED_DOCUMENT_LIBRARY_IDS_KEY] = success;
+            return returnMessage;
+
+        }
+    }
+}
