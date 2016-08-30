d44a175f4f26d374e98c04bbe26493ae0a04fbe3
lbengzon <leandro_bengzon@brown.edu>
2016-08-30 15:12:52 -0400

    Update access controls server fix

--- NusysServer/RequestHandlers/UpdateLibraryElementRequestHandler.cs
+++ NusysServer/RequestHandlers/UpdateLibraryElementRequestHandler.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using NusysIntermediate;
 using NusysServer.Util.SQLQuery;
+using NusysServer.Util;
+using Newtonsoft.Json;
 
 namespace NusysServer
 {
@@ -29,12 +31,19 @@ namespace NusysServer
             {
                 throw new Exception("An Updatelibrary element Request must have an library ID to update");
             }
-            ForwardMessage(message, senderHandler);
             if (message.GetBool(NusysConstants.UPDATE_LIBRARY_ELEMENT_REQUEST_SAVE_TO_SERVER_BOOLEAN, true))
             {
                 SaveToServer(message);
             }
-            
+            //if the access is being updated, let others know of the "new" library element.
+            if (!string.IsNullOrEmpty(message.GetString(NusysConstants.LIBRARY_ELEMENT_ACCESS_KEY)))
+            {
+                ForwardNewLibraryElementToOthers(message.GetString(NusysConstants.UPDATE_LIBRARY_ELEMENT_REQUEST_LIBRARY_ELEMENT_ID), senderHandler);
+            }
+            else
+            {
+                ForwardMessage(message, senderHandler);
+            }
             var returnMessage = new Message();
             returnMessage[NusysConstants.REQUEST_SUCCESS_BOOL_KEY] = true;
 
@@ -96,5 +105,46 @@ namespace NusysServer
             }
         }
 
+        /// <summary>
+        /// This method will fetch all the necessary data from the library element, metadata, and properties sql tables, create a new json serialized
+        /// library element and forward it to other clients with the new library element request type. This should only be called when the users update the library element
+        /// changing it from private to public. 
+        /// </summary>
+        private void ForwardNewLibraryElementToOthers(string libraryId, NuWebSocketHandler senderHandler)
+        {
+            SqlJoinOperationArgs libraryElementJoinProperties = new SqlJoinOperationArgs();
+            libraryElementJoinProperties.LeftTable = new SingleTable(Constants.SQLTableType.LibraryElement);
+            libraryElementJoinProperties.RightTable = new SingleTable(Constants.SQLTableType.Properties);
+            libraryElementJoinProperties.JoinOperator = Constants.JoinedType.LeftJoin;
+            libraryElementJoinProperties.Column1 = Constants.GetFullColumnTitle(Constants.SQLTableType.LibraryElement,
+                NusysConstants.LIBRARY_ELEMENT_LIBRARY_ID_KEY).First();
+            libraryElementJoinProperties.Column2 = Constants.GetFullColumnTitle(Constants.SQLTableType.Properties,
+                NusysConstants.PROPERTIES_LIBRARY_OR_ALIAS_ID_KEY).First();
+            JoinedTable propertiesJoinLibraryElement = new JoinedTable(libraryElementJoinProperties);
+
+            SqlJoinOperationArgs libraryElementJoinPropertiesJoinMetadata = new SqlJoinOperationArgs();
+            libraryElementJoinPropertiesJoinMetadata.LeftTable = propertiesJoinLibraryElement;
+            libraryElementJoinPropertiesJoinMetadata.RightTable = new SingleTable(Constants.SQLTableType.Metadata);
+            libraryElementJoinPropertiesJoinMetadata.JoinOperator = Constants.JoinedType.LeftJoin;
+            libraryElementJoinPropertiesJoinMetadata.Column1 = Constants.GetFullColumnTitle(Constants.SQLTableType.LibraryElement,
+                NusysConstants.LIBRARY_ELEMENT_LIBRARY_ID_KEY).First();
+            libraryElementJoinPropertiesJoinMetadata.Column2 = Constants.GetFullColumnTitle(Constants.SQLTableType.Metadata,
+                NusysConstants.METADATA_LIBRARY_ELEMENT_ID_COLUMN_KEY).First();
+            JoinedTable propertiesJoinLibraryElementJoinMetadata = new JoinedTable(libraryElementJoinPropertiesJoinMetadata);
+
+            var conditionalForLibraryElementId = new SqlQueryEquals(Constants.SQLTableType.LibraryElement, NusysConstants.LIBRARY_ELEMENT_LIBRARY_ID_KEY, libraryId);
+            var query = new SQLSelectQuery(propertiesJoinLibraryElementJoinMetadata, conditionalForLibraryElementId);
+            var libraryElementReturnedMessages = query.ExecuteCommand();
+            PropertiesAndMetadataParser propertiesAndMetadataParser = new PropertiesAndMetadataParser();
+            var libraryElementConcatPropertiesMessages = propertiesAndMetadataParser.ConcatPropertiesAndMetadata(new List<Message>(libraryElementReturnedMessages)).First();
+
+            var libraryElementModel = LibraryElementModelFactory.CreateFromMessage(Constants.StripTableNames(libraryElementConcatPropertiesMessages));
+            var modelJson = JsonConvert.SerializeObject(libraryElementModel);
+            Message forwardMessage = new Message();
+            forwardMessage[NusysConstants.NEW_LIBRARY_ELEMENT_REQUEST_RETURNED_LIBRARY_ELEMENT_MODEL_KEY] = modelJson;
+            Request createNewLibraryElementRequest = new Request(NusysConstants.RequestType.CreateNewLibraryElementRequest, forwardMessage);
+            forwardMessage = createNewLibraryElementRequest.GetFinalMessage();
+            ForwardMessage(forwardMessage, senderHandler);
+        }
     }
 }
