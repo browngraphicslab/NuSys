84d17372ce8b46420f81916a6eecb4a1fe96d694
murrayLuke <luke_murray@brown.edu>
2016-12-02 10:41:06 -0500

    starting the recording node

---
+++ NuSysApp/Components/NuSysRenderer/UI/RecordingNode/RecordingNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+
+namespace NuSysApp
+{
+    public class RecordingNode : RectangleUIElement
+    {
+        public RecordingNode(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
+        {
+        }
+    }
+}
