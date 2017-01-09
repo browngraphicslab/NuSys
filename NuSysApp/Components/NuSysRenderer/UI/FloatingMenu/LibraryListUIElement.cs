f57514302147b47feac5bc760b3fdef48fb86dbb
Miranda Chao <miranda_chao@brown.edu>
2017-01-09 12:31:51 -0500

    example commented out for centered popup

--- NuSysApp/Components/NuSysRenderer/UI/FloatingMenu/LibraryListUIElement.cs
+++ NuSysApp/Components/NuSysRenderer/UI/FloatingMenu/LibraryListUIElement.cs
@@ -104,10 +104,10 @@ namespace NuSysApp
         /// </summary>
         public FilterMenu FilterMenu { get; }
 
-        /// <summary>
-        /// TEST BUTTON
-        /// </summary>
-        private RectangleButtonUIElement _testbutton;
+        ///// <summary>
+        ///// TEST BUTTON
+        ///// </summary>
+        //private RectangleButtonUIElement _testbutton;
 
         public LibraryListUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator)
             : base(parent, resourceCreator)
@@ -174,21 +174,21 @@ namespace NuSysApp
             _filterButton.Tapped += OnFilterButtonTapped;
 
             ///TEST BUTTON
-            _testbutton = new RectangleButtonUIElement(this, Canvas, 0, "test");
-            AddChild(_testbutton);
+            //_testbutton = new RectangleButtonUIElement(this, Canvas, 0, "test");
+            //AddChild(_testbutton);
 
-            _testbutton.Tapped += _testbutton_Tapped;
+            //_testbutton.Tapped += _testbutton_Tapped;
 
             // events so that the library list view adds and removes elements dynamically
             SessionController.Instance.ContentController.OnNewLibraryElement += UpdateLibraryListWithNewElement;
             SessionController.Instance.ContentController.OnLibraryElementDelete += UpdateLibraryListToRemoveElement;
         }
 
-        private void _testbutton_Tapped(InteractiveBaseRenderItem item, CanvasPointer pointer)
-        {
-            CenteredPopup test = new CenteredPopup(SessionController.Instance.NuSessionView, Canvas, "this is a test");
-            SessionController.Instance.NuSessionView.AddChild(test);
-        }
+        //private void _testbutton_Tapped(InteractiveBaseRenderItem item, CanvasPointer pointer)
+        //{
+        //    CenteredPopup test = new CenteredPopup(SessionController.Instance.NuSessionView, Canvas, "this is a test");
+        //    SessionController.Instance.NuSessionView.AddChild(test);
+        //}
 
         /// <summary>
         /// Event handler for when the text of the library search bar changes
