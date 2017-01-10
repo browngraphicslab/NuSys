fddc7779bece93a1d25cf82a16de9edba57b5b46
Vanya Cohen <vanya_cohen@brown.edu>
2017-01-10 11:40:37 -0500

    Adds classes for inkable regions.

---
+++ NuSysApp/Components/NuSysRenderer/UI/InkableUIElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+
+namespace NuSysApp
+{
+    class InkableUIElement : RectangleUIElement
+    {
+        private IInkController _inkController;
+        public InkableUIElement(IInkController inkController, BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
+        {
+            _inkController = inkController;
+            PenPointerPressed += OnPenPointerPressed_Callback;
+            PenPointerDragged += OnPenPointerDragged_Callback;
+            PenPointerReleased += OnPenPointerReleased_Callback;
+        }
+
+        private void OnPenPointerPressed_Callback(InteractiveBaseRenderItem item, CanvasPointer pointer)
+        {
+
+        }
+
+        private void OnPenPointerDragged_Callback(InteractiveBaseRenderItem item, CanvasPointer pointer)
+        {
+
+        }
+
+        private void OnPenPointerReleased_Callback(InteractiveBaseRenderItem item, CanvasPointer pointer)
+        {
+
+        }
+    }
+}
