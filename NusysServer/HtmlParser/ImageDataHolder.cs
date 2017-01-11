9b6189f2b347244660411c6ed374a241c4caccd8
Trent Green <trent_green@brown.edu>
2017-01-11 18:56:22 -0500

    starting merge with sahil

---
+++ NusysServer/HtmlParser/ImageDataHolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NusysIntermediate;
+
+namespace NusysServer
+{
+    public class ImageDataHolder : DataHolder
+    {
+        public Uri Uri { get; set; }
+        public ImageDataHolder(Uri uri, String title) : base(DataType.Image,title)
+        {
+            Uri = uri;
+        }
+    }
+}
