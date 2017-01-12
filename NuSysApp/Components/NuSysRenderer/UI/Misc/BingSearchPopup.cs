5af5a723451647498c1760efa363380148be89fb
Sahil <slymishra@gmail.com>
2017-01-12 13:44:54 -0500

    added ui for popup, some fixes need to be made with focus and luke said he'd handle it

---
+++ NuSysApp/Components/NuSysRenderer/UI/Misc/BingSearchPopup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Microsoft.Graphics.Canvas;
+
+namespace NuSysApp
+{
+    class BingSearchPopup : PopupUIElement
+    {
+        private ScrollableTextboxUIElement _searchbar;
+        private ButtonUIElement _addResults;
+        private GridLayoutManager _layoutManager;
+
+        public BingSearchPopup(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
+        {
+            //set appearance
+            Background = Colors.White;
+            BorderWidth = 2;
+            Bordercolor = Constants.DARK_BLUE;
+            Height = 50;
+
+            _searchbar = new ScrollableTextboxUIElement(this, resourceCreator, false, false);
+            AddChild(_searchbar);
+
+            _addResults = new ButtonUIElement(this, ResourceCreator)
+            {
+                ButtonText = "Add",
+                BorderWidth = 1
+            };
+            AddChild(_addResults);
+
+            _layoutManager = new GridLayoutManager(this,ResourceCreator)
+            {
+             Height   = Height,
+             Width = Width,
+            };
+            _layoutManager.AddRows(new List<float>() {1});
+            _layoutManager.AddColumns(new List<float>() {5,1});
+            _layoutManager.AddElement(_searchbar,0,1);
+            _layoutManager.AddElement(_addResults,0,1);
+            
+            _addResults.Tapped += _addResults_Tapped;
+        }
+
+        private void _addResults_Tapped(InteractiveBaseRenderItem item, CanvasPointer pointer)
+        {
+            //TODO MAKE REQUEST
+            DismissPopup();
+        }
+
+        public override void Update(Matrix3x2 parentToLocalTransform)
+        {
+
+            //arrange items
+            _layoutManager.Height = Height;
+            _layoutManager.Width = Width;
+            base.Update(parentToLocalTransform);
+        }
+
+    }
+}
