a687e702ed10fba28708cf2da9e681b0bf7a4300
Luke Murray <luke_murray@brown.edu>
2017-01-18 15:10:17 -0500

    scrolling in the textbox isn't perfect but it is now good enough

--- NuSysApp/Components/NuSysRenderer/UI/SessionView/ChatBox/ChatBoxUIElement.cs
+++ NuSysApp/Components/NuSysRenderer/UI/SessionView/ChatBox/ChatBoxUIElement.cs
@@ -36,6 +36,11 @@ namespace NuSysApp
 
         private float _newMessageYOffset;
 
+        /// <summary>
+        /// True if we want to scroll to the bottom of the page
+        /// </summary>
+        private bool _scrollToBottom;
+
         public ChatBoxUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
         {
             // set the min height and min width
@@ -185,6 +190,14 @@ namespace NuSysApp
             UpdateScrollAreaSize();
 
             base.Update(parentLocalToScreenTransform);
+
+
+            if (_scrollToBottom)
+            {
+                _readingRect.Scrollto(ScrollingCanvas.ScrollTo.Bottom);
+                _scrollToBottom = false;
+            }
+
         }
 
         /// <summary>
@@ -206,6 +219,13 @@ namespace NuSysApp
             var messageBox = GetMessageBox(chatMessage);
             messageBox.Callback = callback;
             AddChat(headerGrid, messageBox);
+
+            // if the chat box is currently hidden or the chat was sent by the current user
+            // then scroll the chat down
+            if (!IsVisible || user.UserID == WaitingRoomView.UserID)
+            {
+                _scrollToBottom = true;
+            }
         }
 
 
@@ -220,6 +240,14 @@ namespace NuSysApp
             var headerGrid = GetHeaderGrid(user);
             var messageBox = GetMessageBox(chatMessage);
             AddChat(headerGrid, messageBox);
+
+            // if the chat box is currently hidden or the chat was sent by the current user
+            // then scroll the chat down
+            if (!IsVisible || user.UserID == WaitingRoomView.UserID)
+            {
+                _scrollToBottom = true;
+            }
+
         }
 
         /// <summary>
@@ -239,11 +267,8 @@ namespace NuSysApp
             // add the element to the scroling canvas
             _readingRect.AddElement(messageBox, new Vector2(0, _newMessageYOffset));
 
-            // increase the y offset by the new messages height
+            // increment the message y offset by the height of the messageBox box
             _newMessageYOffset += messageBox.Height;
-
-            // set the scroll area size so that it can contain the new message
-            UpdateScrollAreaSize();
         }
 
         /// <summary>
