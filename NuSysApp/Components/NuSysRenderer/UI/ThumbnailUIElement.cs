a759fafe6e2f657a8929607e8fba2bdbda1dedc8
mirandachao <miranda_chao@brown.edu>
2017-03-01 19:11:15 -0500

    thumbnail fix

---
+++ NuSysApp/Components/NuSysRenderer/UI/ThumbnailUIElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Microsoft.Graphics.Canvas;
+
+namespace NuSysApp
+{
+
+    public class ThumbnailUIElement : RectangleUIElement
+    {
+        /// <summary>
+        /// The image to be displayed on the rectangle
+        /// </summary>
+        public ICanvasImage Image { get; set; }
+
+        /// <summary>
+        /// The bounds to draw the image in the rectangle, these are normalized coordinates
+        /// </summary>
+        public override Rect? ImageBounds { get; set; }
+
+        public Rect? RegionBounds { get; set; }
+
+        public ThumbnailUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
+        {
+
+        }
+    }
+}
