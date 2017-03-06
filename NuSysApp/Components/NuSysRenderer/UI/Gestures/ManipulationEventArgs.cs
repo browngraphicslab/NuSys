5f0c8190a115d277eb9e17f0355f100a5071569c
murrayLuke <luke_murray@brown.edu>
2017-03-05 19:07:15 -0500

    manipulation gesture recognizer in progress

---
+++ NuSysApp/Components/NuSysRenderer/UI/Gestures/ManipulationEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuSysApp
+{
+    class ManipulationEventArgs : GestureEventArgs
+    {
+        public float Angle { get; set; }
+        public float ScaleDelta { get; set; }
+        public Vector2 Focus { get; set; }
+    }
+}
---
+++ NuSysApp/Components/NuSysRenderer/UI/Gestures/ManipulationGestureRecognizer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using MyToolkit.Converters;
+
+namespace NuSysApp
+{
+    class ManipulationGestureRecognizer : GestureRecognizer
+    {
+        private Dictionary<uint, Vector2> _pointerIdToPosition;
+
+        private ManipulationEventArgs _manipulationArgs;
+
+        public void ProcessDownEvent(FrameworkElement sender, PointerRoutedEventArgs args)
+        {
+            if (!_pointerIdToPosition.ContainsKey(args.Pointer.PointerId))
+            {
+                _pointerIdToPosition[args.Pointer.PointerId] = args.GetCurrentPoint(sender).Position.ToSystemVector2();
+            }
+        }
+
+        public void ProcessMoveEvents(FrameworkElement sender, PointerRoutedEventArgs args)
+        {
+            _pointerIdToPosition[args.Pointer.PointerId] = args.GetCurrentPoint(sender).Position.ToSystemVector2();
+
+            if (_pointerIdToPosition.Count < 2)
+            {
+                return;
+            }
+
+            var sum = _pointerIdToPosition.Values.Aggregate(Vector2.Zero, (current, point) => current + point);
+            var focus = sum / _pointerIdToPosition.Count;
+            var devSum = _pointerIdToPosition.Values.Aggregate(Vector2.Zero, (current, point) => current + Vector2.Abs(point - focus));
+            var dev = devSum / _pointerIdToPosition.Count;
+        }
+
+        public void ProcessUpEvent(FrameworkElement sender, PointerRoutedEventArgs args)
+        {
+            _pointerIdToPosition.Remove(args.Pointer.PointerId);
+            if (_pointerIdToPosition.Count < 2)
+            {
+                
+            }
+        }
+
+        public void ProcessMouseWheelEvent(FrameworkElement sender, PointerRoutedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessExitedEvent(FrameworkElement sender, PointerRoutedEventArgs args)
+        {
+            _pointerIdToPosition.Remove(args.Pointer.PointerId);
+        }
+
+
+    }
+}
