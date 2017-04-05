67c016de965468e46efc7aa1c446e27e24c0a2fc
Tyler Schicke <tyler_schicke@brown.edu>
2017-04-05 18:38:10 -0400

    added basic TabControl and TabPage classes without drawing support

---
+++ NuSysApp/Components/NuSysRenderer/DetailView/TabAndPageFramework/TabControlUIElement.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+using Windows.Foundation;
+
+namespace NuSysApp.Components.NuSysRenderer.DetailView.TabAndPageFramework
+{
+    class TabControlUIElement : RectangleUIElement
+    {
+        /// <summary>
+        /// List of tabs in the TabControl
+        /// </summary>
+        protected List<TabPageUIElement> _tabs = new List<TabPageUIElement>();
+
+        public TabPageUIElement SelectedTab
+        {
+            get; set;
+        }
+        
+        public int SelectedIndex
+        {
+            get
+            {
+                return SelectedTab == null ? -1 : _tabs.IndexOf(SelectedTab);
+            }
+            set
+            {
+                SelectedTab = _tabs[value];
+            }
+        }
+
+        /// <summary>
+        /// Default constructor for TabControl
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="resourceCreator"></param>
+        public TabControlUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
+        {
+        }
+
+        public TabPageUIElement this[int index]
+        {
+            get
+            {
+                return _tabs[index];
+            }
+            set
+            {
+                _tabs[index] = value;
+            }
+        }
+
+        public TabPageUIElement this[string name]
+        {
+            get
+            {
+                return GetTabWithName(name);
+            }
+        }
+
+        public TabPageUIElement GetTabWithName(string name)
+        {
+            return _tabs.Find((TabPageUIElement t) => t.Name.Equals(name));
+        }
+
+        public int GetTabIndexWithName(string name)
+        {
+            return _tabs.FindIndex((TabPageUIElement t) => t.Name.Equals(name));
+        }
+
+        void AddTab(TabPageUIElement newTab, bool select = false)
+        {
+            _tabs.Add(newTab);
+            if(select)
+            {
+                SelectedTab = newTab;
+            }
+        }
+
+        void RemoveTab(TabPageUIElement tab)
+        {
+            if(SelectedTab == tab)
+            {
+                DeselectTab(tab);
+                if(SelectedTab == tab)
+                {
+                    SelectedTab = null;
+                }
+            }
+            _tabs.Remove(tab);
+        }
+
+        void RemoveTabAt(int index)
+        {
+            if(SelectedIndex == index)
+            {
+                DeselectTabAt(index);
+                if (SelectedIndex == index)
+                {
+                    SelectedTab = null;
+                }
+            }
+            TabPageUIElement t = _tabs[index];
+            _tabs.RemoveAt(index);
+        }
+
+        void RemoveTabWithName(string name)
+        {
+            TabPageUIElement t = GetTabWithName(name);
+            RemoveTab(t);
+        }
+
+        void Clear()
+        {
+            _tabs.Clear();
+            SelectedTab = null;
+        }
+
+        void SelectTab(TabPageUIElement tab)
+        {
+            SelectedTab = tab;
+        }
+
+        void SelectTabAt(int index)
+        {
+            SelectedIndex = index;
+        }
+
+        void SelectTabWithName(string name)
+        {
+            SelectedTab = GetTabWithName(name);
+        }
+
+        void DeselectTab(TabPageUIElement tab)
+        {
+            DeselectTabAt(_tabs.IndexOf(tab));
+        }
+
+        void DeselectTabAt(int index)
+        {
+            SelectedIndex = (index + 1) % _tabs.Count;
+        }
+
+        void DeselectTabWithName(string name)
+        {
+            DeselectTab(GetTabWithName(name));
+        }
+
+        Rect GetTabLocalBounds(int index)
+        {
+            return _tabs[index].GetLocalBounds();
+        }
+        
+        Rect GetTabScreenBounds(int index)
+        {
+            return _tabs[index].GetScreenBounds();
+        }
+
+        void SwapTabs(int index1, int index2)
+        {
+            TabPageUIElement t = _tabs[index1];
+            _tabs[index1] = _tabs[index2];
+            _tabs[index2] = t;
+        }
+
+        void MoveTab(int oldIndex, int newIndex)
+        {
+            TabPageUIElement t = _tabs[oldIndex];
+            _tabs.RemoveAt(oldIndex);
+            _tabs.Insert(newIndex, t);
+        }
+
+    }
+}
---
+++ NuSysApp/Components/NuSysRenderer/DetailView/TabAndPageFramework/TabPageUIElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+
+namespace NuSysApp.Components.NuSysRenderer.DetailView.TabAndPageFramework
+{
+    class TabPageUIElement : RectangleUIElement
+    {
+        public string Name
+        {
+            get; set;
+        }
+
+        public TabPageUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator, string name) : base(parent, resourceCreator)
+        {
+            Name = name;
+        }
+
+        public TabPageUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator, string name, RectangleUIElement content) : base(parent, resourceCreator)
+        {
+            Name = name;
+            AddChild(content);
+        }
+    }
+}
