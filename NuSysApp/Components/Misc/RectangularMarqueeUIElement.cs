999c9b90df66b37029e76ca5b832ca120633c7dd
mirandachao <miranda_chao@brown.edu>
2017-04-09 19:16:13 -0400

    added marchign ants to nusys nodes

---
+++ NuSysApp/Components/Misc/RectangularMarqueeUIElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Geometry;
+
+namespace NuSysApp
+{
+
+    public class RectangularMarqueeUIElement : RectangleUIElement
+    {
+
+        private const float MAX_OFFSET = 15f;
+
+        private float _dashOffset = 0f;
+
+
+        public RectangularMarqueeUIElement(BaseRenderItem parent, ICanvasResourceCreatorWithDpi resourceCreator) : base(parent, resourceCreator)
+        {
+
+        }
+
+        public override void Draw(CanvasDrawingSession ds)
+        {
+            var strokeStyle = new CanvasStrokeStyle
+            {
+                DashCap = CanvasCapStyle.Flat,
+                DashStyle = CanvasDashStyle.Dash,
+                DashOffset = _dashOffset,
+            };
+            ds.DrawRectangle(GetLocalBounds(), Colors.SlateGray, 3f, strokeStyle);
+            base.Draw(ds);
+        }
+
+        public override void Update(Matrix3x2 parentLocalToScreenTransform)
+        {
+            _dashOffset += 0.25f;
+
+            if (_dashOffset > MAX_OFFSET)
+            {
+                _dashOffset = 0;
+            }
+
+            base.Update(parentLocalToScreenTransform);
+        }
+    }
+}
